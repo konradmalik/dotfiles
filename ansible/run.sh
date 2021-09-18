01b4645355eb1257a9e39e06bb8f83edd3c2df66
Konrad Malik <konrad.malik@gmail.com>
2021-09-18 16:32:53 +0200

    update

--- ansible/run.sh
+++ ansible/run.sh
@@ -6,4 +6,9 @@ export PYTHONUNBUFFERED=1
 # show ANSI-colored output
 export ANSIBLE_FORCE_COLOR=true
 
-ansible-playbook -vv -i inventory.yaml -i ../private/inventory.yaml ./playbook.yml --extra-vars "ansible_become_pass=$1"
+ansible-playbook -vv \
+    -i inventory.yaml \
+    -i ../private/inventory.yaml \
+    ./playbook.yml \
+    --extra-vars "variable_host=remote" \
+    --extra-vars "ansible_become_pass=$1"
