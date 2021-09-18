0d83058f12c25559339ce6609edf5a0d7c1ac0a9
Konrad Malik <konrad.malik@gmail.com>
2021-09-18 16:15:12 +0200

    add inventory

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 # stream output
@@ -7,4 +6,4 @@ export PYTHONUNBUFFERED=1
 # show ANSI-colored output
 export ANSIBLE_FORCE_COLOR=true
 
-ansible-playbook -vv -i "localhost," -c local ./playbook.yml --extra-vars "ansible_become_pass=$1"
+ansible-playbook -vv -i inventory.yaml -i ../private/inventory.yaml ./playbook.yml --extra-vars "ansible_become_pass=$1"
