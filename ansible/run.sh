8a8347897d21a8c7574da7406b9e11dec3356f32
Konrad Malik <konrad.malik@gmail.com>
2021-11-11 14:39:27 +0100

    add variable hosts to run.sh

--- ansible/run.sh
+++ ansible/run.sh
@@ -6,10 +6,12 @@ export PYTHONUNBUFFERED=1
 # show ANSI-colored output
 export ANSIBLE_FORCE_COLOR=true
 
+TARGET_HOSTS="${TARGET_HOSTS:-remote}"
+
 ansible-playbook \
     -i inventory.yaml \
     -i ../private/ansible/inventory.yaml \
     ./playbook.yml \
-    --extra-vars "variable_host=remote" \
+    --extra-vars "variable_host=$TARGET_HOSTS" \
     --ask-become-pass
 
