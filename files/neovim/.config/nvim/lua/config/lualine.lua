dcbb9482a9d480ba4e30ba37b035d5097c3986e9
Konrad Malik <konrad.malik@gmail.com>
2021-12-10 11:06:52 +0100

    update lualine config

--- files/neovim/.config/nvim/lua/config/lualine.lua
+++ files/neovim/.config/nvim/lua/config/lualine.lua
@@ -8,7 +8,7 @@ require('lualine').setup{
     sections = {
         lualine_a = { {'mode', upper = true} },
         lualine_b = { {'branch', icon = ''} },
-        lualine_c = { {'filename', file_status = true}, {'diagnostics', sources = {'nvim_lsp'}} },
+        lualine_c = { {'filename', file_status = true}, {'diagnostics', sources = {'nvim_diagnostic'}} },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'hostname' },
         lualine_z = { 'progress', 'location' },
