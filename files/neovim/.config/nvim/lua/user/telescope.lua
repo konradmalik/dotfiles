27a70cb5a1c49a0517fb5b33efcb89e6c86c301c
Konrad Malik <konrad.malik@gmail.com>
2022-01-03 21:59:09 +0100

    setup telescope

--- files/neovim/.config/nvim/lua/user/telescope.lua
+++ files/neovim/.config/nvim/lua/user/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require('telescope')
+telescope.setup{}
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 telescope.load_extension('fzf')
