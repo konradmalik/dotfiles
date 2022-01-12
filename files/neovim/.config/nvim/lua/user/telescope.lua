cb8e5d40bbce6deb2cd03c470cae11be72a1da13
Konrad Malik <konrad.malik@gmail.com>
2022-01-12 11:42:49 +0100

    reorder

--- files/neovim/.config/nvim/lua/user/telescope.lua
+++ files/neovim/.config/nvim/lua/user/telescope.lua
@@ -10,7 +10,7 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap('n', '<leader>ft', "<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>", opts)
 keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+keymap('n', '<leader>ft', "<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>", opts)
