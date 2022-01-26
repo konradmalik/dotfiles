0899b39a9606a109a26508d40ed2b4b9a33bbcc9
Konrad Malik <konrad.malik@gmail.com>
2022-01-26 15:37:15 +0100

    fix lsp keymaps for null-ls

--- files/neovim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ files/neovim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -4,12 +4,14 @@ if not status_ok then
 	return
 end
 
+local lsp_handlers = require("user.lsp.handlers")
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
 	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
+		on_attach = lsp_handlers.on_attach,
+		capabilities = lsp_handlers.capabilities,
 	}
 
 	if server.name == "pylsp" then
--- files/neovim/.config/nvim/lua/user/lsp/null-ls.lua
+++ files/neovim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -1,5 +1,6 @@
 -- https://github.com/jose-elias-alvarez/null-ls.nvim
 local null_ls = require("null-ls")
+local lsp_handlers = require("user.lsp.handlers")
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
@@ -12,4 +13,7 @@ null_ls.setup({
 		formatting.stylua,
 		diagnostics.shellcheck,
 	},
+    -- required to properly register keymaps
+    -- all because null-ls is not recognized (installed) by lsp installer
+    on_attach = lsp_handlers.on_attach,
 })
