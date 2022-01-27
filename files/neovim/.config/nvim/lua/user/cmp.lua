bee61ea698d39456cc3b3bcd64020028f97aa4de
Konrad Malik <konrad.malik@gmail.com>
2022-01-27 19:40:34 +0100

    proper luasnip support

--- files/neovim/.config/nvim/lua/user/cmp.lua
+++ files/neovim/.config/nvim/lua/user/cmp.lua
@@ -53,7 +53,6 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
 		["<Tab>"] = cmp.mapping.select_next_item(),
@@ -67,6 +66,7 @@ cmp.setup({
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
 				buffer = "[Buffer]",
 				path = "[Path]",
 			})[entry.source.name]
@@ -75,6 +75,7 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
 	},
