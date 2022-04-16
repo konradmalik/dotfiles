358d2d9834b6aad6892a12433be7eff24de28a8d
Konrad Malik <konrad.malik@gmail.com>
2022-04-16 12:30:30 +0200

    enable indents

--- files/neovim/.config/nvim/lua/user/treesitter.lua
+++ files/neovim/.config/nvim/lua/user/treesitter.lua
@@ -1,17 +1,12 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
-	ignore_install = {},
-	sync_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-		disable = {
-			"yaml",
-			-- until https://github.com/nvim-treesitter/nvim-treesitter/issues/1136
-			--"python",
-		},
-	},
+    ensure_installed = "all",
+    ignore_install = {},
+    sync_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    },
 })
