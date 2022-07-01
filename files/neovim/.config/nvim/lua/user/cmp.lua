69fb30ab09cab9748bb405702e3ad60a08d51e21
Konrad Malik <konrad.malik@gmail.com>
2022-07-01 19:05:21 +0200

    add cmp for /

--- files/neovim/.config/nvim/lua/user/cmp.lua
+++ files/neovim/.config/nvim/lua/user/cmp.lua
@@ -88,8 +88,12 @@ cmp.setup({
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
-    experimental = {
-        ghost_text = false,
-        native_menu = false,
-    },
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
