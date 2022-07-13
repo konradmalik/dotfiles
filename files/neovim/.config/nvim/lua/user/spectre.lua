d09427c59eb0c570bd4857d090803b79b39cb187
Konrad Malik <konrad.malik@gmail.com>
2022-07-13 12:36:57 +0200

    fix spectre mapping

--- files/neovim/.config/nvim/lua/user/spectre.lua
+++ files/neovim/.config/nvim/lua/user/spectre.lua
@@ -24,5 +24,5 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "<leader>S", spectre.open, opts)
 keymap.set("n", "<leader>sw", function() spectre.open_visual({ select_word = true }) end, opts)
 -- has to be as string, needs to happen at the exact same time!
-keymap.set("v", "<leader>s", "<ESC><CMD>lua require('spectre').open_visual():CR>", opts)
+keymap.set("v", "<leader>s", "<ESC><CMD>lua require('spectre').open_visual()<CR>", opts)
 keymap.set("n", "<leader>sb", spectre.open_file_search, opts)
