33aafc6a143def6ff99317c289a8f42ebd0f9d90
Konrad Malik <konrad.malik@gmail.com>
2022-10-21 08:44:09 +0200

    fix yamlls

--- files/neovim/.config/nvim/lua/konrad/lsp/settings/yamlls.lua
+++ files/neovim/.config/nvim/lua/konrad/lsp/settings/yamlls.lua
@@ -8,15 +8,14 @@ return {
         },
         yaml = {
             format = {
-                enable = false -- prettier
+                enable = false -- use prettier from null-ls instead
             },
-            schemaStore = { enable = true },
+            -- remember about two '*'
             schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = { "/.github/workflows/*" },
+                ["https://json.schemastore.org/github-workflow.json"] = { "/.github/workflows/**" },
                 ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = { "/.gitlab-ci.yml",
-                    "/ci/*", "/gitlabci-templates/*" },
-                -- TODO kubernetes does not work :(
-                ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/all.json"] = { "/k8s/*" },
+                    "/ci/*", "/gitlabci-templates/**" },
+                ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/all.json"] = { "/k8s/**" },
             },
         },
     },
