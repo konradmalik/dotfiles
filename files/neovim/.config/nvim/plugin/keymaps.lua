acdc4cce830e61727454d85acd063b841e0b81a3
Konrad Malik <konrad.malik@gmail.com>
2022-10-30 18:54:38 +0100

    desc keymap example

--- files/neovim/.config/nvim/plugin/keymaps.lua
+++ files/neovim/.config/nvim/plugin/keymaps.lua
@@ -2,6 +2,9 @@ local g = vim.g
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local opts_with_desc = function(desc)
+    return vim.tbl_extend("error", opts, { desc = desc })
+end
 
 -- map leader
 g.mapleader = " "
@@ -15,26 +18,26 @@ g.mapleader = " "
 --   command_mode = "c",
 
 -- to navigate buffers in normal mode
-keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
-keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+keymap.set("n", "<S-h>", ":bprevious<CR>", opts_with_desc("Prev buffer"))
+keymap.set("n", "<S-l>", ":bnext<CR>", opts_with_desc("Next buffer"))
 
 -- quick grep word under the cursor
-keymap.set("n", "<leader>*", ":grep <cword><CR>", opts)
+keymap.set("n", "<leader>*", ":grep <cword><CR>", opts_with_desc("Grep word under cursor"))
 
 local utils = require("konrad.utils");
 -- quickfix niceness
-keymap.set("n", "<C-k>", ":cprevious<CR>", opts)
-keymap.set("n", "<C-j>", ":cnext<CR>", opts)
-keymap.set("n", "<leader>k", ":lprevious<CR>", opts)
-keymap.set("n", "<leader>j", ":lnext<CR>", opts)
-keymap.set("n", "<C-q>", function() utils.ToggleQFList(1) end, opts)
-keymap.set("n", "<leader>q", function() utils.ToggleQFList(0) end, opts)
+keymap.set("n", "<C-k>", ":cprevious<CR>", opts_with_desc("Go to previous QF element"))
+keymap.set("n", "<C-j>", ":cnext<CR>", opts_with_desc("Go to next LL element"))
+keymap.set("n", "<leader>k", ":lprevious<CR>", opts_with_desc("Go to previous LL element"))
+keymap.set("n", "<leader>j", ":lnext<CR>", opts_with_desc("Go to next LL element"))
+keymap.set("n", "<C-q>", function() utils.ToggleQFList(1) end, opts_with_desc("Toggle Quickfix List"))
+keymap.set("n", "<leader>q", function() utils.ToggleQFList(0) end, opts_with_desc("Toggle Local List"))
 
 -- ctrl c as esc in insert mode? why not
-keymap.set("i", "<C-c>", "<esc>", opts)
+keymap.set("i", "<C-c>", "<esc>", opts_with_desc("Ctrl-c as ESC in insert mode"))
 
 -- select last pasted
-keymap.set("n", "<leader>p", "`[v`]", opts)
+keymap.set("n", "<leader>p", "`[v`]", opts_with_desc("Select last pasted"))
 
 -- best remap ever. replace selected by pasting and keep pasted in the register
-keymap.set("v", "<leader>p", "\"_dp", opts)
+keymap.set("v", "<leader>p", "\"_dp", opts_with_desc("Replace selected by pasting and keep pasted in the register"))
