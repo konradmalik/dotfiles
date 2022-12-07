5a353cf040b6042914681e97252fd8a4ac333cec
Konrad Malik <konrad.malik@gmail.com>
2022-12-07 10:04:50 +0100

    clean nvim

--- files/neovim/.config/nvim/after/plugin/cmp.lua
+++ files/neovim/.config/nvim/after/plugin/cmp.lua
@@ -49,7 +49,6 @@ cmp.setup({
         format = function(entry, vim_item)
             -- Kind icons
             vim_item.kind = kind_icons[vim_item.kind]
-            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
             vim_item.menu = ({
                 nvim_lsp = "[LSP]",
                 luasnip = "[Snippet]",
