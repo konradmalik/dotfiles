770e30ca033ceae8dfbe75f7f26affac99162d23
Konrad Malik <konrad.malik@gmail.com>
2022-12-20 18:02:53 +0100

    fix linux zshrc

--- nix/home/konrad/global/linux.nix
+++ nix/home/konrad/global/linux.nix
@@ -42,39 +42,38 @@
     };
     # initExtraFirst is not used in the global file, so we can override here
     initExtraFirst = ''
-      if [ "$(uname)" = "Linux" ]; then
-        if [ -f "/etc/arch-release" ]; then
-            arch-upgrade() {
-                yay -Syu --sudoloop \
-                    --removemake \
-                    --devel \
-                    --nocleanmenu \
-                    --nodiffmenu \
-                    --noeditmenu \
-                    --noupgrademenu \
-                && nix-update \
-                && (flatpak update || true) \
-                && asdf-update
-            }
-            arch-clean() {
-                yay -Sc --noconfirm \
-                && nix-clean
-            }
-        elif [ -f "/etc/debian_version" ]; then
-            ubuntu-upgrade() {
-                sudo apt update \
-                && sudo apt upgrade -y \
-                && sudo snap refresh \
-                && nix-update \
-                && (flatpak update || true) \
-                && asdf-update
-            }
-            ubuntu-clean() {
-                sudo apt autoremove -y \
-                && sudo apt clean \
-                && sudo $HOME/.local/bin/remove-old-snaps.sh \
-                && nix-clean
-            }
+      if [ -f "/etc/arch-release" ]; then
+          arch-upgrade() {
+              yay -Syu --sudoloop \
+                  --removemake \
+                  --devel \
+                  --nocleanmenu \
+                  --nodiffmenu \
+                  --noeditmenu \
+                  --noupgrademenu \
+              && nix-update \
+              && (flatpak update || true) \
+              && asdf-update
+          }
+          arch-clean() {
+              yay -Sc --noconfirm \
+              && nix-clean
+          }
+      elif [ -f "/etc/debian_version" ]; then
+          ubuntu-upgrade() {
+              sudo apt update \
+              && sudo apt upgrade -y \
+              && sudo snap refresh \
+              && nix-update \
+              && (flatpak update || true) \
+              && asdf-update
+          }
+          ubuntu-clean() {
+              sudo apt autoremove -y \
+              && sudo apt clean \
+              && sudo $HOME/.local/bin/remove-old-snaps.sh \
+              && nix-clean
+          }
       fi
     '';
   };
