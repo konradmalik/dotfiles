d5e9e13e9dad55714916a623111f7f1e14a99204
Konrad Malik <konrad.malik@gmail.com>
2022-12-27 20:23:39 +0100

    cachix

--- nix/hosts/presets/programs/nix-shared.nix
+++ nix/hosts/presets/programs/nix-shared.nix
@@ -7,6 +7,8 @@
       experimental-features = "nix-command flakes";
       keep-derivations = true;
       keep-outputs = true;
+      substituters = [ "https://konradmalik.cachix.org" ];
+      trusted-public-keys = [ "konradmalik.cachix.org-1:9REXmCYRwPNL0kAB0IMeTxnMB1Gl9VY5I8w7UVBTtSI=" ];
     };
   };
 }
