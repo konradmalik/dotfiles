e744ac6a47978173d0692cc063bbea10a6a6f759
Konrad Malik <konrad.malik@gmail.com>
2022-12-28 14:29:42 +0100

    add nix-community cachix

--- nix/hosts/presets/programs/nix-shared.nix
+++ nix/hosts/presets/programs/nix-shared.nix
@@ -7,8 +7,14 @@
       experimental-features = "nix-command flakes";
       keep-derivations = true;
       keep-outputs = true;
-      substituters = [ "https://konradmalik.cachix.org" ];
-      trusted-public-keys = [ "konradmalik.cachix.org-1:9REXmCYRwPNL0kAB0IMeTxnMB1Gl9VY5I8w7UVBTtSI=" ];
+      substituters = [
+        "https://konradmalik.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "konradmalik.cachix.org-1:9REXmCYRwPNL0kAB0IMeTxnMB1Gl9VY5I8w7UVBTtSI="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
+      ];
     };
   };
 }
