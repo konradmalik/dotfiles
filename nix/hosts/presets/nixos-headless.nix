7a0e9bed37f08048700ce750e3dd9ceb867908e4
Konrad Malik <konrad.malik@gmail.com>
2023-01-08 14:14:43 +0100

    hostkey rsa deprecated

--- nix/hosts/presets/nixos-headless.nix
+++ nix/hosts/presets/nixos-headless.nix
@@ -64,6 +64,10 @@
     passwordAuthentication = false;
     permitRootLogin = "no";
     ports = [ 22 ];
+    hostKeys = [{
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }];
   };
 
   # Open ports in the firewall.
