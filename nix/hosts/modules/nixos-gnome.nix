b33ffc77260fdcc25e3bddb0f15c86263658d9cb
Konrad Malik <konrad.malik@gmail.com>
2023-01-08 16:26:53 +0100

    pipewire raop

--- nix/hosts/modules/nixos-gnome.nix
+++ nix/hosts/modules/nixos-gnome.nix
@@ -13,8 +13,6 @@
   };
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
