eef484459f554ec3dbcd8011bae9c2962f4b2b32
Konrad Malik <konrad.malik@gmail.com>
2023-01-09 18:55:02 +0100

    nixConfig moved to flake

--- flake.nix
+++ flake.nix
@@ -269,4 +269,15 @@
     {
       devShells.default = pkgs.callPackage ./shell.nix { };
     });
+
+  nixConfig = {
+    extra-substituters = [
+      "https://konradmalik.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "konradmalik.cachix.org-1:9REXmCYRwPNL0kAB0IMeTxnMB1Gl9VY5I8w7UVBTtSI="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
+    ];
+  };
 }
