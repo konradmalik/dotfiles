7d15d1f9c7f4824039a8df3c76b5a2ada3e2d485
Konrad Malik <konrad.malik@gmail.com>
2023-01-09 11:29:08 +0100

    gpg to age

--- shell.nix
+++ shell.nix
@@ -2,25 +2,11 @@ let
   flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
   nixpkgsRev = flakeLock.nodes.nixpkgs.locked.rev;
   lockedNixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz") { };
-  sopsNixRev = flakeLock.nodes.sops-nix.locked.rev;
-  lockedSopsNix = builtins.fetchTarball {
-    url = "https://github.com/Mic92/sops-nix/archive/${sopsNixRev}.tar.gz";
-    sha256 = flakeLock.nodes.sops-nix.locked.narHash;
-  };
 in
-{ pkgs ? lockedNixpkgs, sops-nix ? lockedSopsNix }:
+{ pkgs ? lockedNixpkgs }:
 with pkgs;
 mkShell {
   name = "dotfiles-shell";
-
-  # imports all files ending in .asc/.gpg
-  sopsPGPKeyDirs = [
-    # "${toString ./.}/keys/hosts"
-    "${toString ./nix/secrets}/keys/users"
-  ];
-
-  # stores in .git/gnupg
-  sopsCreateGPGHome = true;
 
   nativeBuildInputs = [
     # nix
@@ -33,7 +19,6 @@ mkShell {
     # yaml
     nodePackages.yaml-language-server
     # sops-nix
-    (pkgs.callPackage sops-nix { }).sops-import-keys-hook
     age
     sops
     ssh-to-age
