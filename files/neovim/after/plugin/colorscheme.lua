19b2b59150a09f184612b4bddb72e9a4e172bc78
Konrad Malik <konrad.malik@gmail.com>
2023-01-15 15:07:00 +0100

    neovim base16

--- files/neovim/after/plugin/colorscheme.lua
+++ files/neovim/after/plugin/colorscheme.lua
@@ -1,18 +1,19 @@
-local catppuccin_ok, catppuccin = pcall(require, "catppuccin")
-if not catppuccin_ok then
+local base16_colorscheme_ok, base16_colorscheme = pcall(require, "base16-colorscheme")
+if not base16_colorscheme_ok then
+    vim.notify("cannot load base16-colorscheme")
     return
 end
 
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-
-catppuccin.setup()
-
--- local colorscheme = "onedark"
--- local colorscheme = "gruvbox"
--- local colorscheme = "dracula"
-local colorscheme = "catppuccin"
-local colorscheme_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not colorscheme_ok then
-    vim.notify("cannot load colorscheme " .. colorscheme)
-    return
+-- injected via nix. If not, we just fallback
+local nix_colors_ok, nix_colors = pcall(require, "konrad.nix-colors")
+if not nix_colors_ok then
+    -- fallback
+    local colorscheme = "base16-catppuccin"
+    local colorscheme_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+    if not colorscheme_ok then
+        vim.notify("cannot load fallback colorscheme " .. colorscheme)
+        return
+    end
 end
+
+base16_colorscheme.setup(nix_colors)
--- nix/home/konrad/common/global/neovim.nix
+++ nix/home/konrad/common/global/neovim.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   # nixified plugins
@@ -152,7 +152,10 @@ in
       vim-sleuth
       which-key-nvim
       # ui
-      catppuccin-nvim
+      {
+        plugin = nvim-base16;
+        optional = false;
+      }
       dressing-nvim
       {
         plugin = neo-tree-nvim;
@@ -165,4 +168,29 @@ in
     source = "${pkgs.dotfiles}/neovim";
     recursive = true;
   };
+
+  xdg.configFile."nvim/lua/konrad/nix-colors.lua".text =
+    let
+      c = config.colorscheme.colors;
+    in
+    ''
+      return {
+          base00 = '#${c.base00}',
+          base01 = '#${c.base01}',
+          base02 = '#${c.base02}',
+          base03 = '#${c.base03}',
+          base04 = '#${c.base04}',
+          base05 = '#${c.base05}',
+          base06 = '#${c.base06}',
+          base07 = '#${c.base07}',
+          base08 = '#${c.base08}',
+          base09 = '#${c.base09}',
+          base0A = '#${c.base0A}',
+          base0B = '#${c.base0B}',
+          base0C = '#${c.base0C}',
+          base0D = '#${c.base0D}',
+          base0E = '#${c.base0E}',
+          base0F = '#${c.base0F}',
+      }
+    '';
 }
