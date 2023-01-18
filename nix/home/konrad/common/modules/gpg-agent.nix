b8890c7dad4ae1672fa72bd7dfeae513feca8845
Konrad Malik <konrad.malik@gmail.com>
2023-01-18 09:04:23 +0100

    fix gpg config

--- nix/home/konrad/common/modules/gpg-agent.nix
+++ nix/home/konrad/common/modules/gpg-agent.nix
@@ -2,45 +2,9 @@
 with lib;
 let
   cfg = config.konrad.programs.gpg-agent;
-in
-{
-  options.konrad.programs.gpg-agent = {
-    enable = mkEnableOption "Enables gpg-agent configuration";
-  };
-
-  config = mkIf cfg.enable {
-    # gpg agent is managed by nix-darwin, but it's not configurable there
-    # home-manager does not support gpg-agent on darwin as a service
-    # but I can migrate once this is done: https://github.com/nix-community/home-manager/pull/2964
-    assertions = [
-      {
-        assertion = !pkgs.stdenv.isDarwin || osConfig.programs.gnupg.agent.enable;
-        message = "gpg-agent must be enabled in nix-darwin for this to work";
-      }
-    ];
-
-    home.file.".gnupg/gpg-agent.conf".text = lib.optionalString pkgs.stdenv.isDarwin ''
-      ## 1-day timeout
-      default-cache-ttl 86400
-      max-cache-ttl 86400
-      # disable smartcard - we don't use it
-      disable-scdaemon
-      # grab mouse and keyboard
-      grab
-      # timeout pinentry (s)
-      pinentry-timeout 30
-      # use tty
-      pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
-    '';
-
-    programs.zsh.initExtra = lib.optionalString pkgs.stdenv.isDarwin ''
-      # gpg agent is started via nix-darwin but GPG_TTY needs to be reset every new interactive shell
-      GPG_TTY="$(tty)"
-      export GPG_TTY
-    '';
-
+  linuxConfig = {
     services.gpg-agent = {
-      enable = !pkgs.stdenv.isDarwin;
+      enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
       enableSshSupport = false;
@@ -55,4 +19,45 @@ in
       '';
     };
   };
+  darwinConfig = {
+    # gpg agent is managed by nix-darwin, but it's not configurable there
+    # home-manager does not support gpg-agent on darwin as a service
+    # but I can migrate once this is done: https://github.com/nix-community/home-manager/pull/2964
+    assertions = [
+      {
+        assertion = osConfig.programs.gnupg.agent.enable;
+        message = "gpg-agent must be enabled in nix-darwin for this to work";
+      }
+    ];
+
+    home.file.".gnupg/gpg-agent.conf".text = ''
+      ## 1-day timeout
+      default-cache-ttl 86400
+      max-cache-ttl 86400
+      # disable smartcard - we don't use it
+      disable-scdaemon
+      # grab mouse and keyboard
+      grab
+      # timeout pinentry (s)
+      pinentry-timeout 30
+      # use tty
+      pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
+    '';
+
+    programs.zsh.initExtra = ''
+      # gpg agent is started via nix-darwin but GPG_TTY needs to be reset every new interactive shell
+      GPG_TTY="$(tty)"
+      export GPG_TTY
+    '';
+  };
+in
+{
+  options.konrad.programs.gpg-agent = {
+    enable = mkEnableOption "Enables gpg-agent configuration";
+  };
+
+  config = mkIf cfg.enable (lib.mkMerge [
+    (mkIf pkgs.stdenv.isDarwin darwinConfig)
+    (mkIf pkgs.stdenv.isLinux linuxConfig)
+  ]);
 }
