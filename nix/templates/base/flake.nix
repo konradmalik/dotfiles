2e25bfb135bd59d92e9f49f718eac0af12cd4aed
Konrad Malik <konrad.malik@gmail.com>
2023-02-12 21:04:42 +0100

    fix: improved template

--- nix/templates/base/flake.nix
+++ nix/templates/base/flake.nix
@@ -3,16 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              unstable = import nixpkgs-unstable {
+                system = final.system;
+                config = final.config;
+              };
+            })
+          ];
+        };
     in
     {
-      devShells = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
-      });
+      devShells = forAllSystems
+        (system:
+          let pkgs = pkgsFor system;
+          in
+          {
+            default = pkgs.mkShell
+              {
+                name = "foo-shell";
+
+                nativeBuildInputs = [
+                  # nix
+                  pkgs.nil
+                  pkgs.nixpkgs-fmt
+                ];
+              };
+          });
     };
 }
