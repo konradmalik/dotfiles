513935ab020e9f806dabb4e0ffa36c034a91875d
Konrad Malik <konrad.malik@gmail.com>
2023-03-14 08:31:26 +0100

    feat(templates): use flake-utils, it's better
    This reverts commit 526b238e0415d79e069c8f6a295af2b8c49ec97a.

--- nix/templates/default/flake.nix
+++ nix/templates/default/flake.nix
@@ -4,50 +4,43 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      mkOverlay = input: name: (final: prev: {
-        "${name}" = import input {
-          system = final.system;
-          config = final.config;
-        };
-      });
-      pkgsFor = system:
-        import nixpkgs {
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        mkOverlay = input: name: (final: prev: {
+          "${name}" = import input {
+            system = final.system;
+            config = final.config;
+          };
+        });
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [
             (mkOverlay nixpkgs-unstable "unstable")
           ];
         };
-    in
-    {
-      devShells = forAllSystems
-        (system:
-          let pkgs = pkgsFor system;
-          in
-          {
-            default = pkgs.mkShell
-              {
-                name = "Shell for this project";
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell
+            {
+              name = "Shell for this project";
 
-                packages = with pkgs; [
-                  # formatters/linters
-
-                  # language-servers
-                  nil
-                ];
-              };
-          });
-      formatter = forAllSystems (system:
-        let pkgs = pkgsFor system;
-        in pkgs.nixpkgs-fmt);
-    };
+              packages = with pkgs; [
+                # formatters/linters
+                formatter
+                # language-servers
+                nil
+              ];
+            };
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
