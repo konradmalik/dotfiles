850712a85060390ae9ef4423442286525c826493
Konrad Malik <konrad.malik@gmail.com>
2023-04-03 10:24:10 +0200

    feat: enable extraPackages for opengl hardware accel

--- nix/hosts/common/optional/wayland-wm.nix
+++ nix/hosts/common/optional/wayland-wm.nix
@@ -42,5 +42,12 @@ in
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+    # https://nixos.wiki/wiki/Accelerated_Video_Playback
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
   };
 }
