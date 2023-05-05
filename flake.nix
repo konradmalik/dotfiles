8516b757dfb8ee99664aedcce72577db10f8dbc2
Konrad Malik <konrad.malik@gmail.com>
2023-05-05 10:53:36 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     };
     nix-colors.url = "github:misterio77/nix-colors";
     hyprland = {
-      url = "github:hyprwm/Hyprland/main";
+      url = "github:hyprwm/Hyprland/v0.25.0";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
