afa3253ce7a6cc0a0a753437bedaf08b0a40175d
Konrad Malik <konrad.malik@gmail.com>
2023-07-05 12:07:02 +0200

    feat: add --include by default to curl

--- home/konrad/common/global/curl.nix
+++ home/konrad/common/global/curl.nix
@@ -1,7 +1,8 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   xdg.configFile.".curlrc".text = ''
-    --write-out "\ndnslookup: %{time_namelookup}\nconnect: %{time_connect}\nappconnect: %{time_appconnect}\npretransfer: %{time_pretransfer}\nstarttransfer: %{time_starttransfer}\ntotal: %{time_total}\nsize: %{size_download}\n"
+    --include
+    --write-out "Stats:\ndnslookup: %{time_namelookup}\nconnect: %{time_connect}\nappconnect: %{time_appconnect}\npretransfer: %{time_pretransfer}\nstarttransfer: %{time_starttransfer}\ntotal: %{time_total}\nsize: %{size_download}\n"
   '';
   home.packages = [ pkgs.curl ];
 }
