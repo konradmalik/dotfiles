d3150468c16502245d864f455c9213dc50216f78
Konrad Malik <konrad.malik@gmail.com>
2023-07-20 13:12:44 +0200

    chore: nix flake update, major changes in my neovim

--- .nvim.lua
+++ .nvim.lua
@@ -2,5 +2,7 @@ local lsp = require("konrad.lsp")
 lsp.add("nil_ls")
 -- lsp.add("nixd")
 
-local null = require("null-ls")
-lsp.add("null-ls", null.builtins.formatting.nixpkgs_fmt)
+lsp.add("null-ls", function(null)
+    return {
+        null.builtins.formatting.nixpkgs_fmt
+    } end)
