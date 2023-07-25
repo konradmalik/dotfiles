5342d5fda855851aabf9f53c45d074bdf1e8b300
Konrad Malik <konrad.malik@gmail.com>
2023-07-25 16:43:13 +0200

    fix: wezterm config changes

--- home/konrad/common/modules/wezterm.nix
+++ home/konrad/common/modules/wezterm.nix
@@ -108,29 +108,31 @@ in
 
         local act = wezterm.action
         config.keys = {
-        { key = ")",        mods = "CTRL",  action = act.ResetFontSize },
-        { key = "-",        mods = "CTRL",  action = act.DecreaseFontSize },
-        { key = "=",        mods = "CTRL",  action = act.IncreaseFontSize },
+            { key = ")",        mods = "CTRL",  action = act.ResetFontSize },
+            { key = "-",        mods = "CTRL",  action = act.DecreaseFontSize },
+            { key = "=",        mods = "CTRL",  action = act.IncreaseFontSize },
 
-        { key = "Enter",    mods = "ALT",  action = act.ToggleFullScreen },
+            { key = "Enter",    mods = "ALT",  action = act.ToggleFullScreen },
 
-        { key = "N",        mods = "CTRL",  action = act.SpawnTab("DefaultDomain") },
-        { key = "n",        mods = "SUPER",  action = act.SpawnTab("DefaultDomain") },
-        { key = "{",        mods = "CTRL",  action = act.ActivateTabRelative(-1) },
-        { key = "{",        mods = "SUPER",  action = act.ActivateTabRelative(-1) },
-        { key = "}",        mods = "CTRL",  action = act.ActivateTabRelative(1) },
-        { key = "}",        mods = "SUPER",  action = act.ActivateTabRelative(1) },
+            { key = "N",        mods = "CTRL",  action = act.SpawnTab("DefaultDomain") },
+            { key = "n",        mods = "SUPER",  action = act.SpawnTab("DefaultDomain") },
+            { key = "{",        mods = "CTRL",  action = act.ActivateTabRelative(-1) },
+            { key = "{",        mods = "SUPER",  action = act.ActivateTabRelative(-1) },
+            { key = "}",        mods = "CTRL",  action = act.ActivateTabRelative(1) },
+            { key = "}",        mods = "SUPER",  action = act.ActivateTabRelative(1) },
 
-        { key = "P",        mods = "CTRL",  action = act.ActivateCommandPalette },
+            { key = "P",        mods = "CTRL",  action = act.ActivateCommandPalette },
+            { key = "p",        mods = "SUPER",  action = act.ActivateCommandPalette },
+            { key = "R",        mods = "CTRL",  action = act.ShowLauncher },
+            { key = "r",        mods = "SUPER",  action = act.ShowLauncher },
 
-        { key = "V",        mods = "CTRL",  action = act.PasteFrom("Clipboard") },
-        { key = "v",        mods = "SUPER",  action = act.PasteFrom("Clipboard") },
-        { key = "Paste",    mods = "NONE",  action = act.PasteFrom("Clipboard") },
+            { key = "V",        mods = "CTRL",  action = act.PasteFrom("Clipboard") },
+            { key = "v",        mods = "SUPER",  action = act.PasteFrom("Clipboard") },
+            { key = "Paste",    mods = "NONE",  action = act.PasteFrom("Clipboard") },
 
-        { key = "C",        mods = "CTRL",  action = act.CopyTo("Clipboard") },
-        { key = "c",        mods = "SUPER",  action = act.CopyTo("Clipboard") },
-        { key = "Copy",     mods = "NONE",  action = act.CopyTo("Clipboard") },
-
+            { key = "C",        mods = "CTRL",  action = act.CopyTo("Clipboard") },
+            { key = "c",        mods = "SUPER",  action = act.CopyTo("Clipboard") },
+            { key = "Copy",     mods = "NONE",  action = act.CopyTo("Clipboard") },
         }
 
         return config
