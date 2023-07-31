53751b7c227553ea0554cc29ab24f958a26ded63
Konrad Malik <konrad.malik@gmail.com>
2023-07-31 19:45:55 +0200

    feat: enable restic on mbp13

--- home/konrad/mbp13.nix
+++ home/konrad/mbp13.nix
@@ -19,4 +19,12 @@
     # installed via homebrew
     package = null;
   };
+  konrad.programs.restic = {
+    enable = true;
+    includes = [
+      "${config.home.homeDirectory}/Code/scratch"
+      "${config.home.homeDirectory}/Documents"
+      "${config.home.homeDirectory}/Library/Mobile Documents/iCloud~md~obsidian/Documents"
+    ];
+  };
 }
