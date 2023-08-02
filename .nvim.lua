682363259569915436622de975c200de21614305
Konrad Malik <konrad.malik@gmail.com>
2023-08-02 10:38:55 +0200

    chore: neovim update

--- .nvim.lua
+++ .nvim.lua
@@ -1,23 +1,3 @@
 require("konrad.lsp").setup({
-    -- "ansiblels",
-    efm = {
-        'nixpkgs_fmt',
-        -- 'terraform_fmt'
-    },
-    -- "gopls",
-    -- "jsonls",
-    -- "lua_ls",
     "nil_ls",
-    -- "omnisharp",
-    -- "pyright",
-    -- "rust_analyzer",
-    -- "terraformls",
-    -- "yamlls",
 })
-
--- require("konrad.dap").setup({
---     "cs",
---     "go",
---     "python",
--- })
-
