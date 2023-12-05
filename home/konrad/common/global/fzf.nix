2f0b1cd6f2d84b854111447f5d097b2e3151d808
Konrad Malik <konrad.malik@gmail.com>
2023-12-05 20:50:06 +0100

    fix: fzf changedir has a typo

--- home/konrad/common/global/fzf.nix
+++ home/konrad/common/global/fzf.nix
@@ -10,7 +10,7 @@
     fileWidgetOptions = [
       "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range=:200 {}'"
     ];
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type -d";
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
     changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
     colors =
       let c = config.colorscheme.colors;
