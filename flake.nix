7ff3edbed52719c8b8c190352e574b87aa04b9f6
Konrad Malik <konrad.malik@gmail.com>
2024-01-26 11:31:23 +0100

    feat: add darwin config check

--- flake.nix
+++ flake.nix
@@ -170,6 +170,12 @@
           modules = [ ./home/konrad/generic.nix ];
         };
       };
+
+      checks = {
+        x86_64-darwin = {
+          mbp13 = self.darwinConfigurations.mbp13.system;
+        };
+      };
     };
 
   nixConfig = {
