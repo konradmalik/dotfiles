2eeb3a20bcd8f7f3c1218951e91a177b81f875ea
Konrad Malik <konrad.malik@gmail.com>
2024-03-17 11:40:14 +0100

    refactor: cleanup atuin a bit

--- home/konrad/common/global/atuin.nix
+++ home/konrad/common/global/atuin.nix
@@ -3,12 +3,10 @@
     enable = true;
     flags = [ "--disable-up-arrow" ];
     settings = {
-      auto_sync = true;
-      keymap_mode = "auto";
-      search_mode = "fuzzy";
       style = "compact";
       sync_frequency = "5m";
       update_check = false;
+      workspaces = true;
     };
   };
 }
