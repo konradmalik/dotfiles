318e7459f920a886caff50ed27cdf13de3793e30
Konrad Malik <konrad.malik@gmail.com>
2024-04-22 18:21:44 +0200

    chore: remove workaround

--- home/konrad/common/optional/desktop/common/default.nix
+++ home/konrad/common/optional/desktop/common/default.nix
@@ -13,29 +13,20 @@
   # silently override mimeapps
   xdg.configFile."mimeapps.list".force = true;
 
-  home =
-    # TODO remove after https://github.com/NixOS/nixpkgs/pull/305711 lands in nixos-unstable
-    let
-      mechanize = pkgs.python311Packages.mechanize.overridePythonAttrs (old: { doCheck = false; });
-      calibre_fix = pkgs.calibre.override
-        (old: {
-          python3Packages = old.python3Packages // { inherit mechanize; };
-        });
-    in
-    {
-      packages = with pkgs; [
-        bitwarden
-        calibre_fix
-        obsidian
-        signal-desktop
-        slack
-        spotify
-        tdesktop
-        # for xdg-open in 'gx' in vim for example
-        xdg-utils
-        zathura
-      ];
-    };
+  home = {
+    packages = with pkgs; [
+      bitwarden
+      calibre
+      obsidian
+      signal-desktop
+      slack
+      spotify
+      tdesktop
+      # for xdg-open in 'gx' in vim for example
+      xdg-utils
+      zathura
+    ];
+  };
 
   konrad.wallpaper = "${customArgs.files}/wallpapers/bishal-mishra.jpg";
 }
