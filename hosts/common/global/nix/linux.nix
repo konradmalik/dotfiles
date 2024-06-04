6465fe4180f5174e1df13dbc4250f60503cf5de2
Konrad Malik <konrad.malik@gmail.com>
2024-06-04 10:23:50 +0200

    chore: NIX_PATH and registry.json is now set by default for flake systems

--- hosts/common/global/nix/linux.nix
+++ hosts/common/global/nix/linux.nix
@@ -3,6 +3,7 @@ let
   # Map registries to channels
   # very useful when using legacy commands (they use NIX_PATH and this is what we are building here)
   # also make sure that no imperative channels are in use: nix-channel --list should be empty
+  # this happens by default in flake-based nixos systems, but here we have a generic linux
   nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 in
 {
