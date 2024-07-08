6d337fb2575706b719d23bd923ab82a9c8cb1770
Konrad Malik <konrad.malik@gmail.com>
2024-07-08 21:56:26 +0200

    feat: atuin sync v2

--- home/konrad/common/modules/base/atuin.nix
+++ home/konrad/common/modules/base/atuin.nix
@@ -4,9 +4,12 @@
     flags = [ "--disable-up-arrow" ];
     settings = {
       style = "compact";
-      sync_frequency = "5m";
+      sync_frequency = "15m";
       update_check = false;
       workspaces = true;
+      sync = {
+        records = true;
+      };
     };
   };
 }
