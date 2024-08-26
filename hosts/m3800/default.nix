00d821b4995325b7701e04538fab5e6110786b65
Konrad Malik <konrad.malik@gmail.com>
2024-08-26 09:10:19 +0200

    fix: workaround unknown bug with suspend even if not idle

--- hosts/m3800/default.nix
+++ hosts/m3800/default.nix
@@ -25,8 +25,16 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchDocked = "ignore";
+  };
+
+  # FIXME some bug in either hyprland or somewhere else
+  # suspends even if not idle after that time
   services.logind.extraConfig = ''
-    IdleAction=suspend
+    #IdleAction=suspend
+    IdleAction=ignore
     IdleActionSec=30min
   '';
 }
