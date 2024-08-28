abaac9dceeef8cd361a7dbf8189001b1bee829cc
Konrad Malik <konrad.malik@gmail.com>
2024-08-28 10:01:35 +0200

    feat: use delta for common git commands, use difftastic on demand only
    Difftastic is very nice, but hides whitespace changes, but those are important in commits as well.

--- home/konrad/common/modules/base/git/default.nix
+++ home/konrad/common/modules/base/git/default.nix
@@ -7,202 +7,228 @@
 }:
 {
   home.packages = with pkgs; [
+    difftastic
     git-extras
     git-crypt
   ];
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    ignores = [ ".DS_Store" ];
-
-    difftastic = {
+  programs.git =
+    let
+      difft = "${lib.getExe pkgs.difftastic}";
+    in
+    {
       enable = true;
-      display = "side-by-side";
-      background = config.colorscheme.variant;
-    };
+      lfs.enable = true;
+      ignores = [ ".DS_Store" ];
 
-    includes = [
-      {
-        condition = "gitdir/i:~/Code/";
-        contents = {
-          user = {
-            email = "konrad.malik@gmail.com";
-            name = "Konrad Malik";
-            signingKey = "${config.home.homeDirectory}/.ssh/personal.pub";
+      delta = {
+        enable = true;
+        options = {
+          hyperlinks = true;
+          navigate = true;
+          side-by-side = true;
+          syntax-theme = "base16";
+        };
+      };
+
+      includes = [
+        {
+          condition = "gitdir/i:~/Code/";
+          contents = {
+            user = {
+              email = "konrad.malik@gmail.com";
+              name = "Konrad Malik";
+              signingKey = "${config.home.homeDirectory}/.ssh/personal.pub";
+            };
+          };
+        }
+        {
+          condition = "gitdir/i:~/Code/gitlab.cerebredev.com/";
+          contents = {
+            user = {
+              email = "konrad@cerebre.io";
+              name = "Konrad Malik";
+              signingKey = "${config.home.homeDirectory}/.ssh/cerebre.pub";
+            };
+          };
+        }
+      ];
+
+      aliases = {
+        graph = "log --graph --oneline --decorate --abbrev-commit";
+        root = "rev-parse --show-toplevel";
+        unstage = "reset HEAD --";
+        last = "log --name-status HEAD^..HEAD";
+        conflicts = "diff --name-only --diff-filter=U";
+        whatadded = "log --diff-filter=A";
+        prepare-worktree = "!${
+          lib.getExe' (pkgs.callPackage ./git-prepare-worktree { }) "git-prepare-worktree"
+        }";
+
+        a = "add";
+        ap = "add -p";
+
+        # list local branches sorted by last modified
+        bl = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(authoremail)%(objectname:short)%09%(refname:short)' refs/heads";
+        bls = "!git for-each-ref --sort='-authordate' --format='%(refname:short)' refs/heads";
+        # same but remote
+        br = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(authoremail)%(objectname:short)%09%(refname:short)' refs/remotes/origin";
+        brs = "!git for-each-ref --sort='-authordate' --format='%(refname:short)' refs/remotes/origin";
+
+        c = "commit --verbose";
+        ca = "commit --all --verbose";
+        cm = "commit -m";
+        cam = "commit --all -m";
+        m = "commit --amend --verbose";
+
+        d = "diff";
+        dc = "diff --cached";
+        dl = "diff HEAD^..HEAD";
+        ds = "diff --staged";
+        dt = "diff --stat";
+
+        dft = "difftool";
+        dfts = "difftool --staged";
+        dlog = "-c diff.external=${difft} log -p --ext-diff";
+        dshow = "-c diff.external=${difft} show HEAD --ext-diff";
+
+        g = "graph";
+        l = "log";
+
+        f = "fetch";
+        p = "push";
+        pf = "push --force-with-lease";
+        pl = "pull";
+
+        r = "rebase";
+        rc = "rebase --continue";
+        ri = "rebase --interactive";
+        rim = "!git rebase --interactive $(git remote show origin | sed -n '/HEAD branch/s/.*: /origin\\//p')";
+        rir = "!git rebase --interactive $(git rev-parse --abbrev-ref --symbolic-full-name @{u})";
+
+        s = "status --short --branch";
+
+        x = "reset";
+        xh = "reset --hard";
+
+        co = "switch";
+        cob = "switch -c";
+
+        mainbranch = "!git remote show origin | sed -n '/HEAD branch/s/.*: /origin\\//p'";
+        remotebranch = "!git rev-parse --abbrev-ref --symbolic-full-name @{u}";
+
+        # list aliases
+        la = "--list-cmds=alias";
+        # gitignore.io
+        gitignore = "!curl -sL https://www.toptal.com/developers/gitignore/api/$@";
+      };
+
+      extraConfig = {
+        branch = {
+          sort = "-committerdate";
+        };
+
+        color = {
+          ui = true;
+        };
+
+        column = {
+          ui = "auto";
+        };
+
+        core = {
+          autocrlf = "input";
+          fsmonitor = true;
+          untrackedcache = true;
+        };
+
+        commit = {
+          gpgSign = true;
+          verbose = true;
+        };
+
+        diff = {
+          algotithm = "histogram";
+          colormoved = "default";
+          colormovedws = "allow-indentation-change";
+          tool = "difftastic";
+        };
+
+        difftool = {
+          prompt = false;
+          difftastic = {
+            cmd = "${difft} $LOCAL $REMOTE";
           };
         };
-      }
-      {
-        condition = "gitdir/i:~/Code/gitlab.cerebredev.com/";
-        contents = {
-          user = {
-            email = "konrad@cerebre.io";
-            name = "Konrad Malik";
-            signingKey = "${config.home.homeDirectory}/.ssh/cerebre.pub";
+
+        gpg = {
+          format = "ssh";
+          ssh = {
+            allowedSignersFile = "${customArgs.files}/allowed_signers";
           };
         };
-      }
-    ];
 
-    aliases = {
-      graph = "log --graph --oneline --decorate --abbrev-commit";
-      root = "rev-parse --show-toplevel";
-      unstage = "reset HEAD --";
-      last = "log --name-status HEAD^..HEAD";
-      conflicts = "diff --name-only --diff-filter=U";
-      whatadded = "log --diff-filter=A";
-      prepare-worktree = "!${
-        lib.getExe' (pkgs.callPackage ./git-prepare-worktree { }) "git-prepare-worktree"
-      }";
+        fetch = {
+          fsckobjects = true;
+          prune = true;
+          pruneTags = true;
+          writeCommitGraph = true;
+        };
 
-      a = "add";
-      ap = "add -p";
+        help = {
+          autocorrect = 10;
+        };
 
-      # list local branches sorted by last modified
-      bl = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(authoremail)%(objectname:short)%09%(refname:short)' refs/heads";
-      bls = "!git for-each-ref --sort='-authordate' --format='%(refname:short)' refs/heads";
-      # same but remote
-      br = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(authoremail)%(objectname:short)%09%(refname:short)' refs/remotes/origin";
-      brs = "!git for-each-ref --sort='-authordate' --format='%(refname:short)' refs/remotes/origin";
+        init = {
+          defaultBranch = "main";
+        };
 
-      c = "commit --verbose";
-      ca = "commit --all --verbose";
-      cm = "commit -m";
-      cam = "commit --all -m";
-      m = "commit --amend --verbose";
+        merge = {
+          conflictstyle = "zdiff3";
+          keepBackup = false;
+          tool = "vimdiff";
+        };
 
-      d = "diff";
-      dc = "diff --cached";
-      dl = "diff HEAD^..HEAD";
-      ds = "diff --staged";
-      dt = "diff --stat";
+        mergetool = {
+          prompt = false;
+        };
 
-      g = "graph";
-      l = "log";
+        pager = {
+          difftool = true;
+        };
 
-      f = "fetch";
-      p = "push";
-      pf = "push --force-with-lease";
-      pl = "pull";
+        push = {
+          default = "tracking";
+          autoSetupRemote = true;
+          gpgSign = "if-asked";
+        };
 
-      r = "rebase";
-      rc = "rebase --continue";
-      ri = "rebase --interactive";
-      rim = "!git rebase --interactive $(git remote show origin | sed -n '/HEAD branch/s/.*: /origin\\//p')";
-      rir = "!git rebase --interactive $(git rev-parse --abbrev-ref --symbolic-full-name @{u})";
+        rebase = {
+          autosquash = true;
+          rebaseMerges = true;
+          updateRefs = true;
+        };
 
-      s = "status --short --branch";
+        receive = {
+          fsckobjects = true;
+        };
 
-      x = "reset";
-      xh = "reset --hard";
+        rerere = {
+          enabled = true;
+          autoUpdate = true;
+        };
 
-      co = "switch";
-      cob = "switch -c";
+        tag = {
+          gpgSign = true;
+        };
 
-      mainbranch = "!git remote show origin | sed -n '/HEAD branch/s/.*: /origin\\//p'";
-      remotebranch = "!git rev-parse --abbrev-ref --symbolic-full-name @{u}";
+        transfer = {
+          fsckobjects = true;
+        };
 
-      # list aliases
-      la = "--list-cmds=alias";
-      # gitignore.io
-      gitignore = "!curl -sL https://www.toptal.com/developers/gitignore/api/$@";
-    };
-
-    extraConfig = {
-      branch = {
-        sort = "-committerdate";
-      };
-
-      color = {
-        ui = true;
-      };
-
-      column = {
-        ui = "auto";
-      };
-
-      core = {
-        autocrlf = "input";
-        fsmonitor = true;
-        untrackedcache = true;
-      };
-
-      commit = {
-        gpgSign = true;
-        verbose = true;
-      };
-
-      diff = {
-        algotithm = "histogram";
-        colormoved = "default";
-        colormovedws = "allow-indentation-change";
-      };
-
-      gpg = {
-        format = "ssh";
-        ssh = {
-          allowedSignersFile = "${customArgs.files}/allowed_signers";
+        worktree = {
+          guessRemote = true;
         };
       };
-
-      fetch = {
-        fsckobjects = true;
-        prune = true;
-        pruneTags = true;
-        writeCommitGraph = true;
-      };
-
-      help = {
-        autocorrect = 10;
-      };
-
-      init = {
-        defaultBranch = "main";
-      };
-
-      merge = {
-        conflictstyle = "zdiff3";
-        keepBackup = false;
-        tool = "vimdiff";
-      };
-
-      mergetool = {
-        prompt = false;
-      };
-
-      push = {
-        default = "tracking";
-        autoSetupRemote = true;
-        gpgSign = "if-asked";
-      };
-
-      rebase = {
-        autosquash = true;
-        rebaseMerges = true;
-        updateRefs = true;
-      };
-
-      receive = {
-        fsckobjects = true;
-      };
-
-      rerere = {
-        enabled = true;
-        autoUpdate = true;
-      };
-
-      tag = {
-        gpgSign = true;
-      };
-
-      transfer = {
-        fsckobjects = true;
-      };
-
-      worktree = {
-        guessRemote = true;
-      };
     };
-  };
 }
