da26e0ba1b890031d92b1a2ce4ce6fe6c98478a1
Konrad Malik <konrad.malik@gmail.com>
2024-11-11 18:34:51 +0100

    fix: move nix.optimise to non-hm

--- hosts/common/modules/nix/not-hm.nix
+++ hosts/common/modules/nix/not-hm.nix
@@ -1,7 +1,31 @@
-{ inputs, ... }:
 {
-  # should be >= max-jobs
-  nix.nrBuildUsers = 16;
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  nix = {
+    # should be >= max-jobs
+    nrBuildUsers = 16;
+
+    optimise =
+      {
+        automatic = true;
+      }
+      // lib.optionalAttrs (pkgs.stdenvNoCC.isLinux) {
+        dates = [ "Fri *-*-* 10:00:00" ];
+      }
+      // lib.optionalAttrs (pkgs.stdenvNoCC.isDarwin) {
+        interval = [
+          {
+            Hour = 10;
+            Minute = 0;
+            Weekday = 5;
+          }
+        ];
+      };
+  };
   # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
 }
