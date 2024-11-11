6219b9762d68e1f0b0341293317b555b03191501
Konrad Malik <konrad.malik@gmail.com>
2024-11-11 18:34:51 +0100

    fix: move nix.optimise to non-hm

--- hosts/common/modules/nix/shared.nix
+++ hosts/common/modules/nix/shared.nix
@@ -36,22 +36,6 @@ in
       # Setting only non standards here. Eg. "nixpkgs" is set by default.
       nixpkgs-stable.flake = inputs.nixpkgs-stable;
     };
-    optimise =
-      {
-        automatic = true;
-      }
-      // lib.optionalAttrs (pkgs.stdenvNoCC.isLinux) {
-        dates = [ "Fri *-*-* 10:00:00" ];
-      }
-      // lib.optionalAttrs (pkgs.stdenvNoCC.isDarwin) {
-        interval = [
-          {
-            Hour = 10;
-            Minute = 0;
-            Weekday = 5;
-          }
-        ];
-      };
     settings = {
       experimental-features = [
         "nix-command"
