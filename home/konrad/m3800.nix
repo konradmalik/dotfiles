f91039f8c2b0f8176ab2cda08402154ff843d741
Konrad Malik <konrad.malik@gmail.com>
2025-01-23 17:47:10 +0100

    feat: configure nvim-dev globally

--- home/konrad/m3800.nix
+++ home/konrad/m3800.nix
@@ -19,6 +19,7 @@ in
   programs.neovim-pde = {
     notesPath = mkOutOfStoreSymlink "${obsidianPath}/Personal";
     spellPath = mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/github.com/konradmalik/neovim-flake/files/spell";
+    devConfigurationPath = mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/github.com/konradmalik/neovim-flake/config";
   };
   konrad.programs.restic = {
     enable = true;
