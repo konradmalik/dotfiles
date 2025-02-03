8011054bdfc2dfc07cb1686da6d291d399064199
Konrad Malik <konrad.malik@gmail.com>
2025-02-03 15:10:01 +0100

    feat: expose pkgs through flake

--- flake/packages.nix
+++ flake/packages.nix
@@ -8,27 +8,29 @@
   perSystem =
     { pkgs, ... }:
     {
-      packages = pkgs.lib.optionalAttrs (pkgs.stdenvNoCC.isLinux) (
-        let
-          rpiSdCard = "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix";
-          # https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
-          missingKernelModulesFix = {
-            nixpkgs.overlays = [
-              (final: prev: { makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; }); })
+      packages =
+        (import ../pkgs/installable { inherit pkgs; })
+        // pkgs.lib.optionalAttrs (pkgs.stdenvNoCC.isLinux) (
+          let
+            rpiSdCard = "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix";
+            # https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+            missingKernelModulesFix = {
+              nixpkgs.overlays = [
+                (final: prev: { makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; }); })
+              ];
+            };
+            modules = [
+              rpiSdCard
+              missingKernelModulesFix
             ];
-          };
-          modules = [
-            rpiSdCard
-            missingKernelModulesFix
-          ];
-        in
-        {
-          installer-iso = import ./../pkgs/special/installer-iso { inherit pkgs specialArgs; };
-          rpi4-1-sd-image =
-            (self.nixosConfigurations.rpi4-1.extendModules { inherit modules; }).config.system.build.sdImage;
-          rpi4-2-sd-image =
-            (self.nixosConfigurations.rpi4-2.extendModules { inherit modules; }).config.system.build.sdImage;
-        }
-      );
+          in
+          {
+            installer-iso = import ./../pkgs/special/installer-iso { inherit pkgs specialArgs; };
+            rpi4-1-sd-image =
+              (self.nixosConfigurations.rpi4-1.extendModules { inherit modules; }).config.system.build.sdImage;
+            rpi4-2-sd-image =
+              (self.nixosConfigurations.rpi4-2.extendModules { inherit modules; }).config.system.build.sdImage;
+          }
+        );
     };
 }
