9a8861e71604469fb19f74584b676d6aca042018
Konrad Malik <konrad.malik@gmail.com>
2025-03-19 17:05:19 +0100

    feat: use DoH in blocky because it seems like my ISP is intercepting DNS traffic and forcing servers

--- hosts/common/modules/blocky.nix
+++ hosts/common/modules/blocky.nix
@@ -1,52 +1,34 @@
-{ config, ... }:
-let
-  publicDns = [
-    #Google
-    "8.8.8.8"
-    "8.8.4.4"
-    #Control D
-    "76.76.2.0"
-    "76.76.10.0"
-    #Quad9
-    "9.9.9.9"
-    "149.112.112.112"
-    #OpenDNS Home
-    "208.67.222.222"
-    "208.67.220.220"
-    #Cloudflare
-    "1.1.1.1"
-    "1.0.0.1"
-    #AdGuard DNS
-    "94.140.14.14"
-    "94.140.15.15"
-    #CleanBrowsing
-    "185.228.168.9"
-    "185.228.169.9"
-    #Alternate DNS
-    "76.76.19.19"
-    "76.223.122.150"
-  ];
-in
 {
   services.blocky = {
     enable = true;
     settings = {
       ports = {
         dns = 53;
-        tls = 853;
         http = 4000;
       };
       upstreams = {
         init.strategy = "failOnError";
-        groups = {
-          default =
-            # TODO enable once unbound works
-            # if config.services.unbound.enable then
-            #   [ "127.0.0.1:${toString config.services.unbound.settings.server.port}" ]
-            # else
-            publicDns;
-        };
+        groups.default = [
+          "https://one.one.one.one/dns-query"
+          "https://dns.quad9.net/dns-query"
+        ];
       };
+      bootstrapDns = [
+        {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = [
+            "1.1.1.1"
+            "1.0.0.1"
+          ];
+        }
+        {
+          upstream = "https://dns.quad9.net/dns-query";
+          ips = [
+            "9.9.9.9"
+            "149.112.112.112"
+          ];
+        }
+      ];
       blocking = {
         denylists = {
           ads = [
