ca91a4bbc571c149bb5e06a0515a726cf5855a2b
Konrad Malik <konrad.malik@gmail.com>
2025-04-14 17:50:25 +0200

    fix: update yazi plugins to avoid error

--- home/konrad/common/modules/base/yazi.nix
+++ home/konrad/common/modules/base/yazi.nix
@@ -7,8 +7,8 @@ let
       yazi-plugins = mkYaziPluginGithub {
         owner = "yazi-rs";
         repo = "plugins";
-        rev = "beb586aed0d41e6fdec5bba7816337fdad905a33";
-        hash = "sha256-enIt79UvQnKJalBtzSEdUkjNHjNJuKUWC4L6QFb3Ou4=";
+        rev = "b12a9ab085a8c2fe2b921e1547ee667b714185f9";
+        hash = "sha256-LWN0riaUazQl3llTNNUMktG+7GLAHaG/IxNj1gFhDRE=";
       };
     in
     {
@@ -16,8 +16,8 @@ let
       kanagawa = mkYaziPluginGithub {
         owner = "dangooddd";
         repo = "kanagawa.yazi";
-        rev = "d98f0c3e27299f86ee080294df2722c5a634495a";
-        hash = "sha256-Z/lyXNCSqX0wvCQd39ZedxOGlhRzQ+M0hqzkBEcpxEE=";
+        rev = "31167ed54c9cc935b2fa448d64d367b1e5a1105d";
+        hash = "sha256-phwGd1i/n0mZH/7Ukf1FXwVgYRbXQEWlNRPCrmR5uNk=";
       };
     };
 in
