d8b5c95233ca2f5dafdf63ef43c5d8c89b4db1c2
Konrad Malik <konrad.malik@gmail.com>
2025-04-20 17:18:16 +0200

    fix: use overlay to fix build

--- hosts/common/modules/nix/shared.nix
+++ hosts/common/modules/nix/shared.nix
@@ -22,6 +22,23 @@ in
           };
         }
       )
+      # FIXME after https://github.com/NixOS/nixpkgs/issues/399907
+      (final: prev: {
+        qt6Packages = prev.qt6Packages.overrideScope (
+          _: kprev: {
+            qt6gtk2 = kprev.qt6gtk2.overrideAttrs (_: {
+              version = "0.5-unstable-2025-03-04";
+              src = final.fetchFromGitLab {
+                domain = "opencode.net";
+                owner = "trialuser";
+                repo = "qt6gtk2";
+                rev = "d7c14bec2c7a3d2a37cde60ec059fc0ed4efee67";
+                hash = "sha256-6xD0lBiGWC3PXFyM2JW16/sDwicw4kWSCnjnNwUT4PI=";
+              };
+            });
+          }
+        );
+      })
     ];
     config = {
       allowUnfree = true;
