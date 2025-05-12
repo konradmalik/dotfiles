1a03554a6a582293e506a9266da054292d9c1c5d
Konrad Malik <konrad.malik@gmail.com>
2025-05-12 11:04:23 +0200

    refactor: mako settings

--- home/konrad/common/modules/desktop/common/wayland-wm/mako.nix
+++ home/konrad/common/modules/desktop/common/wayland-wm/mako.nix
@@ -5,20 +5,22 @@ in
 {
   services.mako = {
     enable = true;
-    iconPath =
-      if c.variant == "dark" then
-        "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
-      else
-        "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    font = "${config.fontProfiles.regular.family} ${toString config.fontProfiles.regular.size}";
-    padding = "10,20";
-    anchor = "top-center";
-    width = 400;
-    height = 150;
-    borderSize = 2;
-    defaultTimeout = 12000;
-    backgroundColor = "#${c.palette.base00}dd";
-    borderColor = "#${c.palette.base03}dd";
-    textColor = "#${c.palette.base05}dd";
+    settings = {
+      icon-path =
+        if c.variant == "dark" then
+          "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
+        else
+          "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
+      font = "${config.fontProfiles.regular.family} ${toString config.fontProfiles.regular.size}";
+      padding = "10,20";
+      anchor = "top-center";
+      width = 400;
+      height = 150;
+      border-size = 2;
+      default-timeout = 12000;
+      background-color = "#${c.palette.base00}dd";
+      border-color = "#${c.palette.base03}dd";
+      text-color = "#${c.palette.base05}dd";
+    };
   };
 }
