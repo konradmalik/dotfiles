5d9294caf3f15c2bfaf0ef49c76127da64193614
Konrad Malik <konrad.malik@gmail.com>
2025-05-18 19:41:34 +0200

    refactor: split system configurations

---
+++ flake/configurations.nix
@@ -0,0 +1,51 @@
+{ inputs, ... }:
+{
+  flake =
+    let
+      specialArgs = {
+        inherit inputs;
+        customArgs = {
+          files = ./../files;
+        };
+      };
+    in
+    {
+      darwinConfigurations = {
+        mbp13 = inputs.darwin.lib.darwinSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/mbp13 ];
+        };
+      };
+
+      nixosConfigurations = {
+        m3800 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/m3800 ];
+        };
+        xps12 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/xps12 ];
+        };
+        vaio = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/vaio ];
+        };
+        rpi4-1 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/rpi4-1 ];
+        };
+        rpi4-2 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/rpi4-2 ];
+        };
+      };
+
+      homeConfigurations = {
+        "konrad@generic" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = specialArgs;
+          modules = [ ./../home/konrad/generic.nix ];
+        };
+      };
+    };
+}
--- flake/default.nix
+++ flake/default.nix
@@ -1,61 +1,14 @@
-{ inputs, ... }:
 {
   imports = [
     ./checks.nix
+    ./configurations.nix
     ./devshells.nix
     ./packages.nix
   ];
 
-  flake =
-    let
-      specialArgs = {
-        inherit inputs;
-        customArgs = {
-          files = ./../files;
-        };
-      };
-    in
-    {
-      templates = import ./../templates;
-
-      darwinConfigurations = {
-        mbp13 = inputs.darwin.lib.darwinSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/mbp13 ];
-        };
-      };
-
-      nixosConfigurations = {
-        m3800 = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/m3800 ];
-        };
-        xps12 = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/xps12 ];
-        };
-        vaio = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/vaio ];
-        };
-        rpi4-1 = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/rpi4-1 ];
-        };
-        rpi4-2 = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [ ./../hosts/rpi4-2 ];
-        };
-      };
-
-      homeConfigurations = {
-        "konrad@generic" = inputs.home-manager.lib.homeManagerConfiguration {
-          pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
-          extraSpecialArgs = specialArgs;
-          modules = [ ./../home/konrad/generic.nix ];
-        };
-      };
-    };
+  flake = {
+    templates = import ./../templates;
+  };
 
   perSystem =
     { pkgs, ... }:
