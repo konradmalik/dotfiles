1641f22ac53aab703414f6a7282a6f87b802545f
Konrad Malik <konrad.malik@gmail.com>
2025-05-18 19:26:47 +0200

    feat: don't import nixpkgs

--- flake/default.nix
+++ flake/default.nix
@@ -1,12 +1,4 @@
 { inputs, ... }:
-let
-  specialArgs = {
-    inherit inputs;
-    customArgs = {
-      files = ./../files;
-    };
-  };
-in
 {
   imports = [
     ./checks.nix
@@ -14,56 +6,60 @@ in
     ./packages.nix
   ];
 
-  flake = {
-    templates = import ./../templates;
+  flake =
+    let
+      specialArgs = {
+        inherit inputs;
+        customArgs = {
+          files = ./../files;
+        };
+      };
+    in
+    {
+      templates = import ./../templates;
 
-    darwinConfigurations = {
-      mbp13 = inputs.darwin.lib.darwinSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/mbp13 ];
+      darwinConfigurations = {
+        mbp13 = inputs.darwin.lib.darwinSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/mbp13 ];
+        };
+      };
+
+      nixosConfigurations = {
+        m3800 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/m3800 ];
+        };
+        xps12 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/xps12 ];
+        };
+        vaio = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/vaio ];
+        };
+        rpi4-1 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/rpi4-1 ];
+        };
+        rpi4-2 = inputs.nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ ./../hosts/rpi4-2 ];
+        };
+      };
+
+      homeConfigurations = {
+        "konrad@generic" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = specialArgs;
+          modules = [ ./../home/konrad/generic.nix ];
+        };
       };
     };
-
-    nixosConfigurations = {
-      m3800 = inputs.nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/m3800 ];
-      };
-      xps12 = inputs.nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/xps12 ];
-      };
-      vaio = inputs.nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/vaio ];
-      };
-      rpi4-1 = inputs.nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/rpi4-1 ];
-      };
-      rpi4-2 = inputs.nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ ./../hosts/rpi4-2 ];
-      };
-    };
-
-    homeConfigurations = {
-      "konrad@generic" = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
-        extraSpecialArgs = specialArgs;
-        modules = [ ./../home/konrad/generic.nix ];
-      };
-    };
-  };
 
   perSystem =
-    { system, pkgs, ... }:
+    { pkgs, ... }:
     {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ ];
-      };
-
       formatter = pkgs.nixfmt-rfc-style;
     };
 }
