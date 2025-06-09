94081dc398037243f9984fb8b6f23fe4e62b9f6a
Konrad Malik <konrad.malik@gmail.com>
2025-06-09 23:05:58 +0200

    feat: use yaziPlugins

--- home/konrad/common/modules/base/yazi.nix
+++ home/konrad/common/modules/base/yazi.nix
@@ -2,24 +2,15 @@
 let
   mkYaziPluginGithub = x: pkgs.fetchFromGitHub x;
 
-  plugins =
-    let
-      yazi-plugins = mkYaziPluginGithub {
-        owner = "yazi-rs";
-        repo = "plugins";
-        rev = "b12a9ab085a8c2fe2b921e1547ee667b714185f9";
-        hash = "sha256-LWN0riaUazQl3llTNNUMktG+7GLAHaG/IxNj1gFhDRE=";
-      };
-    in
-    {
-      git = "${yazi-plugins}/git.yazi";
-      kanagawa = mkYaziPluginGithub {
-        owner = "dangooddd";
-        repo = "kanagawa.yazi";
-        rev = "31167ed54c9cc935b2fa448d64d367b1e5a1105d";
-        hash = "sha256-phwGd1i/n0mZH/7Ukf1FXwVgYRbXQEWlNRPCrmR5uNk=";
-      };
+  plugins = {
+    git = pkgs.yaziPlugins.git;
+    kanagawa = mkYaziPluginGithub {
+      owner = "dangooddd";
+      repo = "kanagawa.yazi";
+      rev = "31167ed54c9cc935b2fa448d64d367b1e5a1105d";
+      hash = "sha256-phwGd1i/n0mZH/7Ukf1FXwVgYRbXQEWlNRPCrmR5uNk=";
     };
+  };
 in
 {
   programs.yazi = {
