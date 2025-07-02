f1239ae936ec644b9909affdfa7b96593a60373f
Konrad Malik <konrad.malik@gmail.com>
2025-07-02 16:18:52 +0200

    fix: gnome keyring ssh socket is in different place now

--- home/konrad/common/modules/desktop/common/gnome-keyring.nix
+++ home/konrad/common/modules/desktop/common/gnome-keyring.nix
@@ -4,6 +4,6 @@
     enable = true;
     components = [ "ssh" ];
   };
-  home.sessionVariables.SSH_AUTH_SOCK = "/run/user/$UID/keyring/ssh";
+  home.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/gcr/ssh";
   home.packages = [ pkgs.seahorse ];
 }
