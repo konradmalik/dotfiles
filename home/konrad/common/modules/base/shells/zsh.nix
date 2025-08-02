14b0634e6ce97ac695b1d6a20fab21480c00418f
Konrad Malik <konrad.malik@gmail.com>
2025-08-02 23:20:50 +0200

    fix: zdotDir use absolute paths

--- home/konrad/common/modules/base/shells/zsh.nix
+++ home/konrad/common/modules/base/shells/zsh.nix
@@ -5,100 +5,99 @@
   ...
 }:
 let
-  zshInitContent =
-    ''
-      # beeping is annoying
-      unsetopt beep
+  zshInitContent = ''
+    # beeping is annoying
+    unsetopt beep
 
-      # enable directories stack
-      setopt autopushd           # Push the current directory visited on the stack.
-      setopt pushdignoredups    # Do not store duplicates in the stack.
-      setopt pushdsilent         # Do not print the directory stack after pushd or popd.
+    # enable directories stack
+    setopt autopushd           # Push the current directory visited on the stack.
+    setopt pushdignoredups    # Do not store duplicates in the stack.
+    setopt pushdsilent         # Do not print the directory stack after pushd or popd.
 
-      ## Reduce latency when pressing <Esc> (helps with vi mode)
-      export KEYTIMEOUT=1
+    ## Reduce latency when pressing <Esc> (helps with vi mode)
+    export KEYTIMEOUT=1
 
-      # fix backspace issues according to https://superuser.com/questions/476532/how-can-i-make-zshs-vi-mode-behave-more-like-bashs-vi-mode/533685#533685
-      bindkey "^?" backward-delete-char
+    # fix backspace issues according to https://superuser.com/questions/476532/how-can-i-make-zshs-vi-mode-behave-more-like-bashs-vi-mode/533685#533685
+    bindkey "^?" backward-delete-char
 
-      # Enable to edit command line in $VISUAL
-      autoload -Uz edit-command-line
-      zle -N edit-command-line
-      bindkey -M vicmd '^v' edit-command-line
+    # Enable to edit command line in $VISUAL
+    autoload -Uz edit-command-line
+    zle -N edit-command-line
+    bindkey -M vicmd '^v' edit-command-line
 
-      #### Functions
-      take() {
-        mkdir -p "$1" && cd "$1"
-      }
+    #### Functions
+    take() {
+      mkdir -p "$1" && cd "$1"
+    }
 
-      groot() {
-        cd "$(git rev-parse --show-toplevel 2>/dev/null)"
-      }
+    groot() {
+      cd "$(git rev-parse --show-toplevel 2>/dev/null)"
+    }
 
-      tmp() {
-        case "$1" in
-          help|h|-h|--help)
-            echo "use 'tmp view' to see tmp folders; without arguments it'll create a new one"
-            ;;
-          view|list|ls)
-            cd /tmp/workspaces && cd $(${pkgs.eza}/bin/exa --sort=modified --reverse | ${pkgs.fzf}/bin/fzf --preview 'ls -A {}') && return 0
-            ;;
-          *)
-            r="/tmp/workspaces/$(${pkgs.unixtools.xxd}/bin/xxd -l3 -ps /dev/urandom)"
-            mkdir -p "$r" && pushd "$r"
-            ;;
-        esac
-      }
+    tmp() {
+      case "$1" in
+        help|h|-h|--help)
+          echo "use 'tmp view' to see tmp folders; without arguments it'll create a new one"
+          ;;
+        view|list|ls)
+          cd /tmp/workspaces && cd $(${pkgs.eza}/bin/exa --sort=modified --reverse | ${pkgs.fzf}/bin/fzf --preview 'ls -A {}') && return 0
+          ;;
+        *)
+          r="/tmp/workspaces/$(${pkgs.unixtools.xxd}/bin/xxd -l3 -ps /dev/urandom)"
+          mkdir -p "$r" && pushd "$r"
+          ;;
+      esac
+    }
 
-      weather() {
-        local param="$1"
-        if [ -z "$param" ]; then
-          curl "wttr.in/?F"
-        else
-          curl "wttr.in/''${param}?F"
-        fi
-      }
+    weather() {
+      local param="$1"
+      if [ -z "$param" ]; then
+        curl "wttr.in/?F"
+      else
+        curl "wttr.in/''${param}?F"
+      fi
+    }
 
-      flakify() {
-        if [ ! -e flake.nix ]; then
-          nix flake new -t github:konradmalik/dotfiles#default .
-        fi
-      }
+    flakify() {
+      if [ ! -e flake.nix ]; then
+        nix flake new -t github:konradmalik/dotfiles#default .
+      fi
+    }
 
-      # credit to https://github.com/MatthewCroughan/nixcfg
-      flash-to() {
-        if [ $(${pkgs.file}/bin/file $1 --mime-type -b) == "application/zstd" ]; then
-          echo "Flashing zst using zstdcat | dd"
-          ( set -x; ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
-        elif [ $(${pkgs.file}/bin/file $2 --mime-type -b) == "application/xz" ]; then
-          echo "Flashing xz using xzcat | dd"
-          ( set -x; ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
-        else
-          echo "Flashing arbitrary file $1 to $2"
-          sudo dd if=$1 of=$2 status=progress conv=sync,noerror bs=64k
-        fi
-      }
+    # credit to https://github.com/MatthewCroughan/nixcfg
+    flash-to() {
+      if [ $(${pkgs.file}/bin/file $1 --mime-type -b) == "application/zstd" ]; then
+        echo "Flashing zst using zstdcat | dd"
+        ( set -x; ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
+      elif [ $(${pkgs.file}/bin/file $2 --mime-type -b) == "application/xz" ]; then
+        echo "Flashing xz using xzcat | dd"
+        ( set -x; ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
+      else
+        echo "Flashing arbitrary file $1 to $2"
+        sudo dd if=$1 of=$2 status=progress conv=sync,noerror bs=64k
+      fi
+    }
 
-      ntfy() {
-        local topic="$1"
-        local message="$2"
-        if [ -z "$topic" ] || [ -z "$message" ]; then
-            echo "usage: $0 <topic> <message>"
-            exit 1
-        fi
-        curl -d "$message" "ntfy.sh/$topic"
-      }
+    ntfy() {
+      local topic="$1"
+      local message="$2"
+      if [ -z "$topic" ] || [ -z "$message" ]; then
+          echo "usage: $0 <topic> <message>"
+          exit 1
+      fi
+      curl -d "$message" "ntfy.sh/$topic"
+    }
 
-      # rfv keybind
-      rfv-widget() { export VISUAL=nvim && ${pkgs.rfv}/bin/rfv }
-      zle -N rfv-widget
-      bindkey '^G' rfv-widget
-    ''
-    + lib.optionalString (pkgs.stdenvNoCC.isDarwin && config.konrad.programs.alacritty.enable) ''
-      # alacritty icon jumping
-      # https://github.com/alacritty/alacritty/issues/2950#issuecomment-706610878
-      printf "\e[?1042l"
-    '';
+    # rfv keybind
+    rfv-widget() { export VISUAL=nvim && ${pkgs.rfv}/bin/rfv }
+    zle -N rfv-widget
+    bindkey '^G' rfv-widget
+  ''
+  + lib.optionalString (pkgs.stdenvNoCC.isDarwin && config.konrad.programs.alacritty.enable) ''
+    # alacritty icon jumping
+    # https://github.com/alacritty/alacritty/issues/2950#issuecomment-706610878
+    printf "\e[?1042l"
+  '';
 
   zcompdumpRemoval = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD rm -f ${config.programs.zsh.dotDir}/.zcompdump
@@ -153,7 +152,7 @@ in
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     autocd = true;
-    dotDir = lib.removePrefix "${config.home.homeDirectory}/" "${config.xdg.configHome}/zsh";
+    dotDir = "${config.xdg.configHome}/zsh";
     defaultKeymap = "viins";
     shellAliases = {
       # cat on steroids
