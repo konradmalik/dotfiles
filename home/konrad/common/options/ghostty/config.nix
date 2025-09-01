f2dd82ab2ecf9b10adcd8a3c17cc57be49aaa399
Konrad Malik <konrad.malik@gmail.com>
2025-09-01 22:58:41 +0200

    feat: ghostty don't ask for quit confirmation
    preferably, i'd like to not ask only if in tmux but not possible

--- home/konrad/common/options/ghostty/config.nix
+++ home/konrad/common/options/ghostty/config.nix
@@ -4,12 +4,12 @@
   theme,
 }:
 ''
-  gtk-single-instance = true
+  confirm-close-surface = false
+  gtk-single-instance = false
   gtk-titlebar = false
   font-family = ${fontFamily}
   font-size = ${toString fontSize}
   mouse-hide-while-typing = true
   theme = ${theme}
-  shell-integration = detect
   shell-integration-features = true
 ''
