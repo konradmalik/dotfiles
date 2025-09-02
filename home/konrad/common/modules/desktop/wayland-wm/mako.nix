6f92eecc74f41d3af5979cd18a01a045e495d22d
Konrad Malik <konrad.malik@gmail.com>
2025-09-02 10:56:33 +0200

    feat: improve mako config

--- home/konrad/common/modules/desktop/wayland-wm/mako.nix
+++ home/konrad/common/modules/desktop/wayland-wm/mako.nix
@@ -8,21 +8,35 @@ in
   services.mako = {
     enable = true;
     settings = {
+      background-color = "#${c.palette.base00}dd";
+      border-color = "#${c.palette.base03}dd";
+      text-color = "#${c.palette.base05}dd";
+
       icon-path =
         if c.variant == "dark" then
           "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
         else
           "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
       font = "${config.fontProfiles.regular.family} ${toString config.fontProfiles.regular.size}";
-      padding = "10,20";
+
       anchor = "top-center";
+      layer = "overlay";
+
       width = 400;
       height = 150;
       border-size = 2;
-      default-timeout = 12000;
-      background-color = "#${c.palette.base00}dd";
-      border-color = "#${c.palette.base03}dd";
-      text-color = "#${c.palette.base05}dd";
+      padding = "10,20";
+
+      default-timeout = 10000;
+      ignore-timeout = false;
+      max-visible = 5;
+      sort = "-time";
+      group-by = "app-name";
+
+      actions = true;
+
+      format = "<b>%s</b>\\n%b";
+      markup = true;
     };
   };
 }
