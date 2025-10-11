36b069dbc60a5e729c62046dcb337188600a4a37
Konrad Malik <konrad.malik@gmail.com>
2025-10-11 21:42:54 +0200

    feat: add framework

---
+++ home/konrad/framework.nix
@@ -0,0 +1,40 @@
+{ config, ... }:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  obsidianPath = "${config.home.homeDirectory}/obsidian";
+in
+{
+  imports = [
+    ./common/nixos.nix
+    ./common/modules/desktop
+  ];
+
+  konrad.programs.bitwarden.enable = true;
+  programs.neovim-pde = {
+    notesPath = mkOutOfStoreSymlink "${obsidianPath}/Personal";
+    spellPath = mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/github.com/konradmalik/neovim-flake/files/spell";
+    devConfigurationPath = mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/github.com/konradmalik/neovim-flake/config";
+  };
+  konrad.programs.restic = {
+    enable = true;
+    includes = [
+      "${config.home.homeDirectory}/Code/scratch"
+      "${config.home.homeDirectory}/Documents"
+      obsidianPath
+    ];
+  };
+
+  konrad.services.syncthing = {
+    enable = true;
+    install = true;
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    # exec-once = [ "hyprctl dispatch dpms off 'eDP-1'" ];
+    # monitor = [ "eDP-1, disable" ];
+    device = {
+      name = "elan-touchscreen";
+      enabled = false;
+    };
+  };
+}
---
+++ hosts/framework/disko.nix
@@ -0,0 +1,64 @@
+{
+  disko.devices.disk = {
+    main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              settings = {
+                allowDiscards = true;
+              };
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "16G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
