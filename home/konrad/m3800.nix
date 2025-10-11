f9850db6bc114e40705c34d756fcf20e5f5c2937
Konrad Malik <konrad.malik@gmail.com>
2025-10-11 21:37:31 +0200

    feat: don't disable m3800 monitor

--- home/konrad/m3800.nix
+++ home/konrad/m3800.nix
@@ -30,8 +30,9 @@ in
   };
 
   wayland.windowManager.hyprland.settings = {
-    exec-once = [ "hyprctl dispatch dpms off 'eDP-1'" ];
-    monitor = [ "eDP-1, disable" ];
+    # exec-once = [ "hyprctl dispatch dpms off 'eDP-1'" ];
+    # monitor = [ "eDP-1, disable" ];
+    monitor = [ "eDP-1, preferred, auto, 2" ];
     device = {
       name = "elan-touchscreen";
       enabled = false;
