47f7966f0b6716021216d05c781b07444f1a9a1e
Konrad Malik <konrad.malik@gmail.com>
2025-10-19 10:01:38 +0200

    feat: tlp settings

--- hosts/common/profiles/laptop.nix
+++ hosts/common/profiles/laptop.nix
@@ -3,7 +3,24 @@
 
   programs.light.enable = true;
   services.thermald.enable = true;
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 40;
+    };
+  };
 
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend";
