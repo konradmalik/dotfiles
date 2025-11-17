4d23628d3314bff573542052d775f9789eb64e62
Konrad Malik <konrad.malik@gmail.com>
2025-11-17 15:46:51 +0100

    feat: use device id in disko

--- hosts/framework/disko.nix
+++ hosts/framework/disko.nix
@@ -2,7 +2,7 @@
   disko.devices.disk = {
     main = {
       type = "disk";
-      device = "/dev/nvme0n1";
+      device = "/dev/disk/by-id/nvme-WD_BLACK_SN7100_1TB_253176801101";
       content = {
         type = "gpt";
         partitions = {
