7e8f99650de156d1e6d1083d32048598ea7a05c9
Konrad Malik <konrad.malik@gmail.com>
2026-01-17 23:59:22 +0100

    fix: tmux first window, then vim

--- .tmux.sh
+++ .tmux.sh
@@ -4,6 +4,6 @@ set -e
 
 session=$1
 
-tmux send-keys -t "$session" "vim flake.nix" Enter
 tmux new-window -t "$session" -c "#{pane_current_path}"
 tmux previous-window -t "$session"
+tmux send-keys -t "$session" "vim flake.nix" Enter
