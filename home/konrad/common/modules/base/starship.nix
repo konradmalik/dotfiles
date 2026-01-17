20535b2a40fa12383150a39479b4cdcca44d9f20
Konrad Malik <konrad.malik@gmail.com>
2026-01-17 23:59:02 +0100

    feat: simplify starship config

--- home/konrad/common/modules/base/starship.nix
+++ home/konrad/common/modules/base/starship.nix
@@ -6,37 +6,11 @@
       command_timeout = 2000;
 
       aws.disabled = true;
+      azure.disabled = true;
+      gcloud.disabled = true;
+
       battery.disabled = true;
       cmd_duration.disabled = true;
-      crystal.disabled = true;
-      dart.disabled = true;
-      docker_context.disabled = true;
-      dotnet.disabled = false;
-      elixir.disabled = true;
-      elm.disabled = true;
-      env_var.disabled = true;
-      erlang.disabled = true;
-      gcloud.disabled = true;
-      golang.disabled = false;
-      hostname.disabled = false;
-      java.disabled = false;
-      jobs.disabled = false;
-      julia.disabled = true;
-      kubernetes.disabled = false;
-      line_break.disabled = false;
-      lua.disabled = false;
-      memory_usage.disabled = true;
-      nim.disabled = true;
-      nix_shell.disabled = false;
-      ocaml.disabled = true;
-      openstack.disabled = true;
-      package.disabled = true;
-      perl.disabled = true;
-      php.disabled = true;
-      purescript.disabled = true;
-      python.disabled = false;
-      ruby.disabled = true;
-      rust.disabled = false;
       shell = {
         disabled = false;
         zsh_indicator = "";
@@ -50,11 +24,6 @@
         # 3 and more is suspicious
         threshold = 3;
       };
-      singularity.disabled = true;
-      swift.disabled = true;
-      terraform.disabled = false;
-      username.disabled = false;
-      zig.disabled = false;
     };
   };
 }
