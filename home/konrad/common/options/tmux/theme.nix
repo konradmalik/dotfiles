1d22cbece49021d332b03b30996f7469a0ab93b8
Konrad Malik <konrad.malik@gmail.com>
2026-02-10 10:07:43 +0100

    feat: tmux center date

--- home/konrad/common/options/tmux/theme.nix
+++ home/konrad/common/options/tmux/theme.nix
@@ -2,6 +2,7 @@
 let
   c = config.lib.stylix.colors;
 in
+# tmux
 ''
   set -g status-style "fg=#${c.base04},bg=default"
   set -g window-status-style "bg=default"
@@ -11,7 +12,8 @@ in
   set -g window-status-activity-style "fg=#${c.base09}"
 
   set -g status-left "#[fg=#${c.base0D},bold]  #S "
-  set -g status-right "#[fg=#${c.base0A},bold]%a %Y-%m-%d 󱑒 %H:%M #[fg=#${c.base0E}] #H"
+  set -g status-right "#[align=absolute-centre] #[fg=#${c.base0A},bold] %a %Y-%m-%d 󱑒 %H:%M #[align=right]"
+  set -ga status-right "#[fg=#${c.base0E}] #H"
   set -g status-justify left
   set -g status-left-length 200     # increase length (from 10)
   set -g status-right-length 200    # increase length (from 10)
