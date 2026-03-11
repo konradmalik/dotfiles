2a7afb321a958116c43f5d2966f2f0f6c22bcb2c
Konrad Malik <konrad.malik@gmail.com>
2026-03-11 13:10:51 +0100

    feat: update block lists

--- hosts/common/options/blocky.nix
+++ hosts/common/options/blocky.nix
@@ -40,19 +40,34 @@
       blocking = {
         denylists = {
           ads = [
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
-            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-            "http://sysctl.org/cameleon/hosts"
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+            # https://github.com/hagezi/dns-blocklists
+            "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.txt"
+            # https://github.com/MajkiIT/polish-ads-filter
+            "https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-pihole-filters/hostfile.txt"
+          ];
+          security = [
+            # https://github.com/hagezi/dns-blocklists#threat-intelligence-feeds
+            "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.txt"
+            # https://cert.pl
+            "https://hole.cert.pl/domains/domains.txt"
+            # https://github.com/FiltersHeroes/KADhosts
+            "https://raw.githubusercontent.com/FiltersHeroes/KADhosts/master/KADomains.txt"
           ];
           fakenews = [
-            "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts"
+            # https://github.com/StevenBlack/hosts
+            "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-only/hosts"
+          ];
+          gambling = [
+            # https://github.com/MajkiIT/polish-ads-filter
+            "https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-pihole-filters/gambling-hosts.txt"
           ];
         };
         clientGroupsBlock = {
           default = [
             "ads"
+            "security"
             "fakenews"
+            "gambling"
           ];
         };
         loading = {
