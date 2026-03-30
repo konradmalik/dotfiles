2cabf60bee22946437140645f9048743d97e31a1
Konrad Malik <konrad.malik@gmail.com>
2026-03-30 07:39:09 +0200

    feat: nvim in tmux

--- .tmux.sh
+++ .tmux.sh
@@ -6,4 +6,4 @@ session=$1
 
 tmux new-window -t "$session" -c "#{pane_current_path}"
 tmux previous-window -t "$session"
-tmux send-keys -t "$session" "vim flake.nix" Enter
+tmux send-keys -t "$session" "nvim flake.nix" Enter
