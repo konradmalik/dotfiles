57dfecd232e728f03e0a01c0d9eb02d4da2db924
Konrad Malik <konrad.malik@gmail.com>
2026-05-31 23:57:07 +0200

    Revert "fix: allow insecure electron"
    This reverts commit 29ab25daec3c9f6ba968303aab14a176c695cca5.

--- hosts/common/modules/nix/shared/default.nix
+++ hosts/common/modules/nix/shared/default.nix
@@ -18,9 +18,7 @@
     ];
     config = {
       allowUnfree = true;
-      permittedInsecurePackages = [
-        "electron-39.8.10"
-      ];
+      permittedInsecurePackages = [ ];
     };
   };
   nix = {
