eccc6641534996297ab85751343ce5647d950ac9
Konrad Malik <konrad.malik@gmail.com>
2026-05-31 23:12:24 +0200

    feat: move state to 26.11

--- home/konrad/common/modules/base/default.nix
+++ home/konrad/common/modules/base/default.nix
@@ -46,7 +46,7 @@
   home = {
     username = lib.mkDefault "konrad";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
-    stateVersion = lib.mkDefault "26.05";
+    stateVersion = lib.mkDefault "26.11";
 
     sessionVariables = {
       LANG = "en_US.UTF-8";
