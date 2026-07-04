982bfc28ae4392cea1ecb5197ac4f008ac9b3bed
Konrad Malik <konrad.malik@gmail.com>
2026-07-04 13:48:13 +0200

    refactor: fzf options renamed

--- home/konrad/common/modules/base/fzf.nix
+++ home/konrad/common/modules/base/fzf.nix
@@ -7,11 +7,15 @@
     enable = true;
     defaultCommand = "${pkgs.fd}/bin/fd --type f";
     defaultOptions = [ "--bind 'ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'" ];
-    fileWidgetCommand = defaultCommand;
-    fileWidgetOptions = [
-      "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range=:200 {}'"
-    ];
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
-    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    fileWidget = {
+      command = defaultCommand;
+      options = [
+        "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range=:200 {}'"
+      ];
+    };
+    changeDirWidget = {
+      command = "${pkgs.fd}/bin/fd --type d";
+      options = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    };
   };
 }
