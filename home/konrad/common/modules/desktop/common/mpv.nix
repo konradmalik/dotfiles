82e8079b24d01b0eac8ed236bd59d513cd2a9457
Konrad Malik <konrad.malik@gmail.com>
2026-07-12 00:09:47 +0200

    fix: workaround for mpv on darwin

--- home/konrad/common/modules/desktop/common/mpv.nix
+++ home/konrad/common/modules/desktop/common/mpv.nix
@@ -40,9 +40,18 @@
   };
   programs.mpv = {
     enable = true;
-    package = pkgs.mpv.override {
-      youtubeSupport = true;
-    };
+    # FIXME: fails to build on aarch64-darwin
+    package =
+      (
+        if pkgs.stdenv.hostPlatform.isDarwin then
+          (builtins.getFlake "github:NixOS/nixpkgs/89570f24e97e614aa34aa9ab1c927b6578a43775")
+          .legacyPackages.${pkgs.system}.mpv
+        else
+          pkgs.mpv
+      ).override
+        {
+          youtubeSupport = true;
+        };
     config = {
       hwdec = "auto";
       loop-playlist = "inf";
