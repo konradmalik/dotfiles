87bdde74251de0421d6aa606e4b731db2aa84558
Konrad Malik <konrad.malik@gmail.com>
2026-07-12 11:10:10 +0200

    fix: darwin workaround for starship

--- home/konrad/common/modules/base/starship.nix
+++ home/konrad/common/modules/base/starship.nix
@@ -1,6 +1,14 @@
-{
+{ pkgs, ... }: {
   programs.starship = {
     enable = true;
+    # FIXME: fails to build on aarch64-darwin
+    package = (
+      if pkgs.stdenv.hostPlatform.isDarwin then
+        (builtins.getFlake "github:NixOS/nixpkgs/89570f24e97e614aa34aa9ab1c927b6578a43775")
+        .legacyPackages.${pkgs.system}.starship
+      else
+        pkgs.starship
+    );
     settings = {
       add_newline = false;
       command_timeout = 2000;
