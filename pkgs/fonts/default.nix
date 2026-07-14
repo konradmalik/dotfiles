f35564b460aa1de08d3afb6e08165755f0123a6b
Konrad Malik <konrad.malik@gmail.com>
2026-07-14 15:19:53 +0200

    Revert "fix: iosevka build on darwin"
    This reverts commit 14e732c75c4550621c7da00aae5683726fd03d55.

--- pkgs/fonts/default.nix
+++ pkgs/fonts/default.nix
@@ -2,14 +2,10 @@ final: prev:
 
 let
   mkNerdFont = prev.callPackage ./mkNerdFont.nix { };
-  # FIXME remove stable workaround once fixed. Unstable fails to build
-  iosevka =
-    (builtins.getFlake "github:NixOS/nixpkgs/97dd352dc415e846fb278b773ff476bb38a80afb")
-    .legacyPackages.${prev.pkgs.system}.iosevka;
 in
 {
   fonts = {
-    iosemka = mkNerdFont (prev.callPackage ./iosemka.nix { inherit iosevka; });
-    iorkeley = mkNerdFont (prev.callPackage ./iorkeley.nix { inherit iosevka; });
+    iosemka = mkNerdFont (prev.callPackage ./iosemka.nix { });
+    iorkeley = mkNerdFont (prev.callPackage ./iorkeley.nix { });
   };
 }
