4d250880c97e44ce90defd632c2a1c7154dde8ee
Konrad Malik <konrad.malik@gmail.com>
2026-08-16 21:28:43 +0200

    feat: autologin

--- hosts/common/modules/dm.nix
+++ hosts/common/modules/dm.nix
@@ -1,7 +1,30 @@
-{ pkgs, ... }:
+{ config, lib, ... }:
+let
+  allHmUsers = builtins.attrNames config.home-manager.users;
+  hyprlandUsers = builtins.filter (
+    user: config.home-manager.users.${user}.wayland.windowManager.hyprland.enable
+  ) allHmUsers;
+in
 {
+  assertions = [
+    {
+      assertion = builtins.length hyprlandUsers <= 1;
+      message = "greetd autologin needs exactly one hyprland user, got: ${toString hyprlandUsers}";
+    }
+  ];
+
   services.greetd = {
     enable = true;
-    settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+    settings = {
+      # required by greetd, only reachable after logging out
+      default_session.command = "${config.services.greetd.package}/bin/agreety --cmd start-hyprland";
+    }
+    // lib.optionalAttrs (hyprlandUsers != [ ]) {
+      # autologin on boot
+      initial_session = {
+        command = "start-hyprland";
+        user = builtins.head hyprlandUsers;
+      };
+    };
   };
 }
