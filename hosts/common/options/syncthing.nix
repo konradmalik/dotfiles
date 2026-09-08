c5af369a61ead9ffce215f878f2c7f8c9222818b
Konrad Malik <konrad.malik@gmail.com>
2026-09-08 22:59:23 +0200

    feat: improve synthing

--- hosts/common/options/syncthing.nix
+++ hosts/common/options/syncthing.nix
@@ -1,19 +1,19 @@
 { config, lib, ... }:
-with lib;
 let
   cfg = config.konrad.services.syncthing;
 in
 {
   options.konrad.services.syncthing = {
-    enable = mkEnableOption "Enables syncthing and its configuration through nixos (uses home-manager as well)";
+    enable = lib.mkEnableOption "Enables syncthing and its configuration through nixos";
 
-    user = mkOption {
-      type = types.str;
+    user = lib.mkOption {
+      type = lib.types.str;
       description = "user name";
     };
 
-    bidirectional = mkOption {
-      type = types.bool;
+    bidirectional = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
       description = "Whether to use receiveonly or sendreceive mode.";
     };
   };
@@ -26,37 +26,35 @@ in
         rpi4-2.id = "I5455AU-F2ZA6OT-O3KAIZS-7UGVDKA-OYF2T7P-RA556GI-FJKGNGN-ARXD5AP";
         x1c6.id = "44QKQXH-FWGCUJB-O4S36QZ-2LJ23DS-GXP5CAC-EIS75BN-EXRSY54-H5TAJAE";
       };
-      otherDevices = lib.filterAttrs (n: _: n != config.networking.hostName) devices;
+      hostName = config.networking.hostName;
+      otherDevices = lib.filterAttrs (n: _: n != hostName) devices;
 
-      homeDirectory = config.home-manager.users.${cfg.user}.home.homeDirectory;
+      homeDirectory = config.users.users.${cfg.user}.home;
+
+      mkFolder = name: {
+        path = "${homeDirectory}/${name}";
+        devices = lib.attrNames otherDevices;
+        type = if cfg.bidirectional then "sendreceive" else "receiveonly";
+      };
     in
-    mkIf cfg.enable {
-      services = {
-        syncthing = {
-          enable = true;
-          guiAddress = "127.0.0.1:8384";
-          user = cfg.user;
-          group = "wheel";
-          dataDir = homeDirectory;
-          configDir = "${config.home-manager.users.${cfg.user}.xdg.configHome}/syncthing";
-          openDefaultPorts = true;
-          overrideDevices = true;
-          overrideFolders = true;
-          settings = {
-            devices = otherDevices;
-            folders = {
-              "Documents" = {
-                path = "${homeDirectory}/Documents";
-                devices = lib.attrNames otherDevices;
-                type = if cfg.bidirectional then "sendreceive" else "receiveonly";
-              };
-              "obsidian" = {
-                path = "${homeDirectory}/obsidian";
-                devices = lib.attrNames otherDevices;
-                type = if cfg.bidirectional then "sendreceive" else "receiveonly";
-              };
-            };
-          };
+    lib.mkIf cfg.enable {
+      assertions = [
+        {
+          assertion = devices ? ${hostName};
+          message = "konrad.services.syncthing: ${hostName} is not in the devices list";
+        }
+      ];
+
+      services.syncthing = {
+        enable = true;
+        guiAddress = "127.0.0.1:8384";
+        user = cfg.user;
+        group = config.users.users.${cfg.user}.group;
+        dataDir = homeDirectory;
+        openDefaultPorts = true;
+        settings = {
+          devices = otherDevices;
+          folders = lib.genAttrs [ "Documents" "obsidian" ] mkFolder;
         };
       };
     };
