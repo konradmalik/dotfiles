04f28669bdc8c1ec0adce26ed62eb58138bf94fd
Konrad Malik <konrad.malik@gmail.com>
2026-09-25 17:19:01 +0200

    feat: add framework hardware key

--- home/konrad/common/modules/base/ssh-keys.nix
+++ home/konrad/common/modules/base/ssh-keys.nix
@@ -7,7 +7,7 @@
       }
       {
         url = "https://gitlab.com/konradmalik.keys";
-        sha256 = "sha256:1rnq9x6jibaplb8sllcy8v9a14d313mkki5q74w1jp2dgrbh9q6g";
+        sha256 = "sha256:1fryqblwfsn5js0wj5jlpiqvqpifd0fwag60wildhnfa8rvgg66s";
       }
     ];
   };
