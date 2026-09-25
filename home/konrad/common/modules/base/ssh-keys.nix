d9c33c883580363f94bd7b9f45bcd7eafc3f877e
Konrad Malik <konrad.malik@gmail.com>
2026-09-25 17:40:40 +0200

    fix: keys sha

--- home/konrad/common/modules/base/ssh-keys.nix
+++ home/konrad/common/modules/base/ssh-keys.nix
@@ -3,7 +3,7 @@
     personal.remotes = [
       {
         url = "https://github.com/konradmalik.keys";
-        sha256 = "sha256:1rnq9x6jibaplb8sllcy8v9a14d313mkki5q74w1jp2dgrbh9q6g";
+        sha256 = "sha256:0v50h4n60sp2fygh14ys3rphzcayjyx5k9jqzpyk2ln32fi16wnp";
       }
       {
         url = "https://gitlab.com/konradmalik.keys";
