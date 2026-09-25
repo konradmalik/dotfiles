759b405b0d44532f83d14ae8281afa0536c5222b
Konrad Malik <konrad.malik@gmail.com>
2026-09-25 17:08:31 +0200

    feat: x1c6 hardware key

--- home/konrad/common/modules/base/ssh-keys.nix
+++ home/konrad/common/modules/base/ssh-keys.nix
@@ -3,11 +3,11 @@
     personal.remotes = [
       {
         url = "https://github.com/konradmalik.keys";
-        sha256 = "sha256:1c7dg2ilvfpwp00945mgdv88ankdvsdxqn0c5f6cl8x82bs22a7w";
+        sha256 = "sha256:1rnq9x6jibaplb8sllcy8v9a14d313mkki5q74w1jp2dgrbh9q6g";
       }
       {
         url = "https://gitlab.com/konradmalik.keys";
-        sha256 = "sha256:1v84r1q46szgs1hyjkj7hpgg8wrjydc42g4gmnwy5h3rv17rzr8i";
+        sha256 = "sha256:1rnq9x6jibaplb8sllcy8v9a14d313mkki5q74w1jp2dgrbh9q6g";
       }
     ];
   };
